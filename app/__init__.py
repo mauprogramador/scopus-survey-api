457da7f48a85739a7a8002f939e48e2b8be11365
Mauricio <sir.silvabmauricio@gmail.com>
2024-02-27 02:54:49 -0400

    feat: update version

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 
-"""Scopus Searcher API"""
+"""
+Scopus Searcher API
 
-__version__ = '1.0.0'
+API for Bibliographic Survey of Scopus Articles
+"""
+
+__version__ = '0.0.0'
 __license__ = 'GPL'
 
-__author__ = 'MauProgramador'
+__author__ = 'mauprogramador'
 __contact__ = 'sir.silvabmauricio@gmail.com'
 
 __status__ = 'Development'
