3262ff3497ca27b7973446a5101d58f952b75a32
mauprogramador <sir.silvabmauricio@gmail.com>
2024-08-20 19:58:35 -0400

    feat: add standard exception and error messages

---
+++ app/core/common/messages.py
@@ -0,0 +1,21 @@
+CONNECTION_ERROR = "Connection error in request"
+CONNECTION_EXCEPTION = "Unexpected error from request <{}>"
+CONNECTION_TIMEOUT = "Request connection timeout"
+
+NOT_FOUND_ERROR = "No articles found"
+SCOPUS_API_ERROR = "Invalid response from Scopus API"
+DECODING_ERROR = "Error in decoding response from Scopus API"
+VALIDATE_ERROR = "Error in validate response from Scopus API"
+INTERRUPT_ERROR = "Unexpected application interruption"
+REQUEST_ERROR = "Validation error in request <{}>"
+RESPONSE_ERROR = "Validation error in response <{}>"
+PYDANTIC_ERROR = "Pydantic validation error: {} validation errors for {}"
+UNEXPECTED_ERROR = "Unexpected error <{}>"
+
+INVALID_ACCESS_TOKEN = "Invalid Access Token"
+INVALID_KEYWORD = "Invalid Keyword"
+INVALID_KEYWORDS_LENGTH = "There must be at least two keywords"
+
+MISSING_ACCESS_TOKEN = "Missing required Access Token header"
+MISSING_API_KEY = "Missing required APIKey query parameter"
+MISSING_KEYWORDS = "Missing required Keywords query parameter"
