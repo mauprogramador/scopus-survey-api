8fb757ddeac2e3f03dda0dddde5fde2fa4d4a65b
mauprogramador <sir.silvabmauricio@gmail.com>
2024-08-21 00:14:48 -0400

    refactor: update fastapi config

--- app/framework/fastapi/config.py
+++ app/framework/fastapi/config.py
@@ -3,77 +3,78 @@ from fastapi.openapi.models import Example
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-from app.core.config.config import TOKEN, TOML
-from app.framework.exceptions.handle_exceptions import (
-    BaseExceptionResponse,
-    ExceptionHandler,
-)
+from app.core.config.config import TOKEN, TOKEN_HEADER, TOML
+from app.framework.exceptions.exception_handler import ExceptionHandler
+from app.framework.exceptions.http_exceptions import BaseExceptionResponse
 from app.framework.fastapi.lifespan import lifespan
-
-TITLE = 'Scopus-Searcher-API'
-VERSION = 'v2.0.0'
-SUMMARY = 'API for Bibliographic Survey of Scopus Articles'
-DESCRIPTION = f"""
-[**Web Application**]({TOML.url}/scopus-searcher/api)
-&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
-[**Documentation**](https://mauprogramador.github.io/scopus-searcher-api/)
-"""
 
 SEARCH_ROUTE_DESCRIPTION = """
 **Keywords:** must be separated by a comma and each keyword can contain
-letters, numbers, underscores and spaces, from 2 to 120 characters <br/><br/>
+letters, numbers, underscores and spaces, from 2 to 50 characters <br/><br/>
 **Ex.:** Python, Machine Learning, Data Science, Neural Networks
 """
-WEB_API_ROUTE_DESCRIPTION = f"""
-**Web API:** <{TOML.url}/scopus-searcher/api>
-"""
-WEB_TABLE_ROUTE_DESCRIPTION = f"""
-**Web Table:** <{TOML.url}/scopus-searcher/api/table>
+
+DESCRIPTION = f"""
+[**Web Application**](/scopus-searcher/api)
+&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+[**Documentation**]({TOML.documentation})
 """
 
+CONTACT = {
+    "name": TOML.name,
+    "url": "https://github.com/mauprogramador",
+    "email": TOML.email,
+}
+LICENSE = {
+    "name": "MIT License",
+    "identifier": "MIT",
+    "url": (
+        "https://github.com/mauprogramador/"
+        "scopus-searcher-api/blob/master/LICENSE"
+    ),
+}
 RESPONSES = {
-    422: {
-        'model': BaseExceptionResponse,
-        'description': 'Base Exception Response',
+    400: {
+        "model": BaseExceptionResponse,
+        "description": "Base exceptions response",
     }
 }
-CONTACT = {
-    'name': 'Mauricio',
-    'email': 'mauricio.batista@estudante.ifms.edu.br',
-}
 FASTAPI = {
-    'title': TITLE,
-    'summary': SUMMARY,
-    'description': DESCRIPTION,
-    'version': VERSION,
-    'docs_url': '/',
-    'responses': RESPONSES,
-    'contact': CONTACT,
-    'exception_handlers': ExceptionHandler().handlers,
-    'lifespan': lifespan,
+    "debug": TOML.debug,
+    "title": TOML.title,
+    "summary": TOML.description,
+    "description": DESCRIPTION,
+    "version": f"v{TOML.version}",
+    "docs_url": "/",
+    "exception_handlers": ExceptionHandler().handlers,
+    "lifespan": lifespan,
+    "contact": CONTACT,
+    "license_info": LICENSE,
+    "responses": RESPONSES,
 }
-TEMPLATES = Jinja2Templates(directory='app/framework/web/templates')
-STATIC_PATH = 'app/framework/web/static'
+
+TEMPLATES = Jinja2Templates(directory="app/framework/web/templates")
 STATIC = {
-    '/styles': StaticFiles(directory=f'{STATIC_PATH}/css'),
-    '/icons': StaticFiles(directory=f'{STATIC_PATH}/icon'),
-    '/images': StaticFiles(directory=f'{STATIC_PATH}/img'),
-    '/scripts': StaticFiles(directory=f'{STATIC_PATH}/js'),
-    '/svgs': StaticFiles(directory=f'{STATIC_PATH}/svg'),
+    "/styles": StaticFiles(directory="app/framework/web/static/css"),
+    "/images": StaticFiles(directory="app/framework/web/static/img"),
+    "/scripts": StaticFiles(directory="app/framework/web/static/js"),
+    "/svgs": StaticFiles(directory="app/framework/web/static/svg"),
 }
+
 DESCRIPTION = (
-    'The value of the X-Access-Token header will be '
-    'set automatically, you **should not** change it'
+    f"The value of the {TOKEN_HEADER} header will be "
+    "set automatically, you **should not** change it"
 )
 OPENAPI_EXAMPLE = {
-    'Token': Example(
-        summary='Access Token', description=DESCRIPTION, value=TOKEN
+    "Token": Example(
+        summary="Access Token", description=DESCRIPTION, value=TOKEN
     )
 }
+
 CORS = {
-    'middleware_class': CORSMiddleware,
-    'allow_origins': ['*'],
-    'allow_credentials': True,
-    'allow_methods': ['GET'],
-    'allow_headers': ['*'],
+    "middleware_class": CORSMiddleware,
+    "allow_origins": ["*"],
+    "allow_credentials": True,
+    "allow_methods": ["GET"],
+    "allow_headers": ["*"],
 }
