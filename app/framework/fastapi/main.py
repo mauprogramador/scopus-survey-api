c8c30dbabfd25e610efdc4dc10ed48c3d0f0e9d2
mauprogramador <sir.silvabmauricio@gmail.com>
2024-08-21 01:18:17 -0400

    feat: add middlewares and mobile qrcode

--- app/framework/fastapi/main.py
+++ app/framework/fastapi/main.py
@@ -4,7 +4,11 @@ from fastapi import FastAPI
 from app.core.config.config import LOG, TOML
 from app.framework.fastapi.config import CORS, FASTAPI, STATIC
 from app.framework.fastapi.routes import router
-from app.framework.middleware.tracing import TraceControl
+from app.framework.middleware import (
+    RedirectNotFoundRoutes,
+    TraceExceptionControl,
+)
+from app.utils.access_qrcode import ShowAccessQRCode
 
 app = FastAPI(**FASTAPI)
 
@@ -14,9 +18,11 @@ for url, static in STATIC.items():
     app.mount(url, static)
 
 app.add_middleware(**CORS)
-app.add_middleware(TraceControl)
+app.add_middleware(RedirectNotFoundRoutes)
+app.add_middleware(TraceExceptionControl)
 
 
 if __name__ == "__main__":
-    LOG.info("Scopus API was initialized 🚀")
+    LOG.info("Scopus Searcher API was initialized 🚀")
+    ShowAccessQRCode(TOML.port)
     uvicorn.run(**TOML.uvicorn)
