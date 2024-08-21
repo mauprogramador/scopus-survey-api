6e101a3ac3ee02de834ed7dcf615b926ff2f27ee
mauprogramador <sir.silvabmauricio@gmail.com>
2024-08-21 00:22:02 -0400

    feat: add header and query types

---
+++ app/framework/fastapi/types.py
@@ -0,0 +1,34 @@
+from fastapi import Header, Query
+
+from app.core.common.patterns import API_KEY_PATTERN, TOKEN_PATTERN
+from app.core.common.types import Keywords
+from app.core.config.config import (
+    API_KEY_HEADER,
+    KEYWORDS_HEADER,
+    TOKEN_HEADER,
+)
+from app.framework.fastapi.config import OPENAPI_EXAMPLE
+
+TokenHeader: str = Header(
+    alias=TOKEN_HEADER,
+    description="The Validation Access Token",
+    min_length=32,
+    max_length=32,
+    pattern=TOKEN_PATTERN,
+    openapi_examples=OPENAPI_EXAMPLE,
+)
+
+APIKeyQuery: str = Query(
+    alias=API_KEY_HEADER,
+    description="Your Scopus APIKey",
+    min_length=32,
+    max_length=32,
+    pattern=API_KEY_PATTERN,
+)
+
+KeywordsQuery: Keywords = Query(
+    alias=KEYWORDS_HEADER,
+    description="Keywords to search for in the articles",
+    min_length=1,
+    max_length=4,
+)
