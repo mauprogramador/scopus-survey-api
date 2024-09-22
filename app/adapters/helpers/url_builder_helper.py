81067c8fee0f8fbdadcdb725036c1545754af8de
mauprogramador <sir.silvabmauricio@gmail.com>
2024-09-22 16:45:10 -0400

    feat: update scopus configs and urls for abstract api

--- app/adapters/helpers/url_builder_helper.py
+++ app/adapters/helpers/url_builder_helper.py
@@ -2,12 +2,12 @@ from urllib.parse import quote_plus
 
 from app.core.common.types import Keywords
 from app.core.config.scopus import (
-    API_URL,
-    ARTICLE_PAGE_URL,
+    ABSTRACT_API_URL,
     BOOLEAN_OPERATOR,
     DATE_RANGE,
     FIELDS,
-    PAGINATION_SUFFIX,
+    PAGINATION_URL,
+    SEARCH_API_URL,
 )
 from app.core.domain.metaclasses import URLBuilder
 
@@ -15,22 +15,18 @@ from app.core.domain.metaclasses import URLBuilder
 class URLBuilderHelper(URLBuilder):
     """Generate and format URLs for HTTP requests"""
 
-    __SEP = ":"
-
     def __init__(self) -> None:
         """Generate and format URLs for HTTP requests"""
         self.__url: str = None
 
     def get_search_url(self, keywords: Keywords) -> str:
         query = quote_plus(BOOLEAN_OPERATOR.join(keywords))
-        self.__url = API_URL.format(
-            query=query, fields=FIELDS, date=DATE_RANGE
-        )
+        self.__url = SEARCH_API_URL.format(query=query, date=DATE_RANGE)
         return self.__url
 
     def get_pagination_url(self, page: int) -> str:
-        return f"{self.__url}{PAGINATION_SUFFIX}{page}"
+        return PAGINATION_URL.format(search_url=self.__url, page=page)
 
-    def get_article_page_url(self, scopus_id: str) -> str:
-        scopus_id = scopus_id.split(self.__SEP)[1]
-        return ARTICLE_PAGE_URL.format(scopus_id=scopus_id)
+    def get_abstract_url(self, url: str) -> str:
+        fields = quote_plus(FIELDS)
+        return ABSTRACT_API_URL.format(abstract_url=url, fields=fields)
