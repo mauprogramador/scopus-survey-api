8621002d13c5945f15e9308301c1ff1ebaa14c5a
mauprogramador <sir.silvabmauricio@gmail.com>
2024-09-22 17:01:51 -0400

    test: add new serializers tests

--- tests/unitary/core/data/test_serializers.py
+++ tests/unitary/core/data/test_serializers.py
@@ -1,37 +1,33 @@
 from math import ceil
 
-from app.core.config.scopus import QUOTA_EXCEEDED
-from app.core.data.serializers import Article, ScopusHeaders, ScopusJSONSchema
-from tests.helpers.data import article
+from app.core.config.scopus import NULL, QUOTA_EXCEEDED
+from app.core.data.serializers import (
+    ScopusArticle,
+    ScopusHeaders,
+    ScopusResult,
+    ScopusSearch,
+)
 from tests.helpers.models import HeadersResponse
+from tests.helpers.utils import abstract
 from tests.mocks.common import SCOPUS_API_JSON
+from tests.mocks.unitary import SCOPUS_ARTICLE_JSON, SCOPUS_RESULT_JSON
 
 
-def test_validate_scopus_json_schema():
-    scopus_json = ScopusJSONSchema.model_validate(SCOPUS_API_JSON)
+def test_validate_scopus_result():
+    model = ScopusResult.model_validate(SCOPUS_RESULT_JSON)
+
+    assert model.link == "true"
+    assert model.url == "any"
+    assert model.scopus_id == "SCOPUS_ID:any"
+
+
+def test_validate_scopus_search():
+    scopus_json = ScopusSearch.model_validate(SCOPUS_API_JSON)
 
     assert scopus_json.total_results == 156
     assert scopus_json.items_per_page == 25
     assert scopus_json.pages_count == ceil(156 / 25)
-    assert scopus_json.entry[0].title == "any"
-    assert len(scopus_json.entry) == 1 and len(scopus_json.articles) == 1
-
-
-def test_validate_article_model():
-    article_model = Article.model_validate(article())
-    article_data = article_model.model_dump(by_alias=True)
-
-    assert len(article_data) == 8
-    assert "@_fa" not in article_data
-    assert article_model.link == "true"
-    assert article_model.url == "any"
-    assert article_model.volume == "any"
-    assert article_model.date == "any"
-    assert article_model.doi == "any"
-    assert article_model.citations == "any"
-    assert article_model.title == "any"
-    assert article_model.scopus_id == "SCOPUS_ID:any"
-    assert article_model.publication_name == "any"
+    assert len(scopus_json.entry) == 0
 
 
 def test_validate_scopus_headers():
@@ -41,3 +37,20 @@ def test_validate_scopus_headers():
     assert QUOTA_EXCEEDED in scopus_headers.status
     assert scopus_headers.reset_datetime
     assert scopus_headers.quota_exceeded
+
+
+def test_validate_scopus_article():
+    model = ScopusArticle.model_validate(abstract())
+
+    assert model.scopus_id == "SCOPUS_ID:any"
+    assert model.url == NULL and model.abstract == NULL
+    value = model.eid + model.title + model.publication_name + model.volume
+    value = value + model.date + model.doi + model.citations + model.authors
+    assert value.count("any") == 8
+
+
+def test_validate_scopus_article_complete_response():
+    model = ScopusArticle.model_validate(SCOPUS_ARTICLE_JSON)
+
+    assert model.abstract == "any"
+    assert model.authors == "any1, any2"
