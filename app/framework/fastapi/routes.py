90c56f1f0725f23818cf0502f50fa914bd0e1840
mauprogramador <sir.silvabmauricio@gmail.com>
2024-12-27 17:26:00 -0400

    feat: add new query params and csv route

--- app/framework/fastapi/routes.py
+++ app/framework/fastapi/routes.py
@@ -1,3 +1,4 @@
+from http import HTTPStatus
 from typing import Annotated
 
 from fastapi import Depends, Request
@@ -5,52 +6,76 @@ from fastapi.responses import FileResponse, HTMLResponse
 from fastapi.routing import APIRouter
 
 from app.adapters.factories.usecase_factory import make_usecase
-from app.adapters.presenters.template_context import TemplateContextBuilder
-from app.core.data.dtos import SearchParams
-from app.framework.dependencies import AccessToken, QueryParams
-from app.framework.fastapi.config import SEARCH_ROUTE_DESCRIPTION, TEMPLATES
+from app.adapters.helpers.template_builder import TemplateBuilder
+from app.adapters.presenters.csv_response import CSVResponse
+from app.core.data.enums import Language
+from app.core.data.validators import SearchParams
+from app.framework.dependencies import AccessToken, SearchQueryParams
+from app.framework.dependencies.api_key_query_param import APIKeyQueryParam
+from app.framework.fastapi.config import SEARCH_ROUTE_DESCRIPTION
+
 
 router = APIRouter(prefix="/scopus-survey/api")
 
 
 @router.get(
-    "/search-articles",
-    status_code=200,
+    "/survey",
+    status_code=HTTPStatus.OK,
     tags=["API"],
-    summary="Downloads the CSV file of found articles",
+    summary="Survey for articles and download the found ones in a CSV file",
     response_description="CSV file of found articles downloaded",
     description=SEARCH_ROUTE_DESCRIPTION,
     response_class=FileResponse,
     dependencies=[Depends(AccessToken())],
 )
-async def search_articles(
-    query_params: Annotated[QueryParams, Depends(QueryParams())]
+async def survey_articles(
+    query_params: Annotated[SearchQueryParams, Depends(SearchQueryParams())]
 ):
-    search_params = SearchParams.model_validate(query_params.to_dict())
+    search_params = SearchParams.model_validate(query_params.items)
     return make_usecase().get_articles(search_params)
 
 
 @router.get(
-    "",
-    status_code=200,
-    tags=["Web"],
-    summary="Renders the application API web page",
-    response_description="application API web page loaded",
-    response_class=HTMLResponse,
+    "/csv",
+    status_code=HTTPStatus.OK,
+    tags=["API"],
+    summary="Download the pre-existing CSV file of the found articles",
+    response_description="CSV file of found articles downloaded",
+    response_class=FileResponse,
+    dependencies=[Depends(AccessToken())],
 )
-async def render_web_application(request: Request):
-    context = TemplateContextBuilder(request).get_web_app_context()
-    return TEMPLATES.TemplateResponse(*context)
+async def download_csv(
+    api_key: Annotated[str, Depends(APIKeyQueryParam())],
+):
+    return CSVResponse.build(api_key)
 
 
 @router.get(
-    "/table",
-    status_code=200,
+    "/{lang}/search-articles",
+    status_code=HTTPStatus.OK,
+    tags=["Web"],
+    summary="Renders the search articles web page",
+    response_description="search articles web page loaded",
+    response_class=HTMLResponse,
+)
+async def render_search_articles_web_page(
+    request: Request,
+    lang: Language,
+):
+    return TemplateBuilder.search_template(request, lang)
+
+
+@router.get(
+    "/{lang}/articles-table",
+    status_code=HTTPStatus.OK,
     tags=["Web"],
     summary="Renders the articles table web page",
     response_description="articles table web page loaded",
     response_class=HTMLResponse,
 )
-async def render_web_table(request: Request):
-    context = TemplateContextBuilder(request).get_table_context()
-    return TEMPLATES.TemplateResponse(*context)
+async def render_articles_table_web_page(
+    request: Request,
+    lang: Language,
+    api_key: Annotated[str, Depends(APIKeyQueryParam())],
+):
+    return TemplateBuilder.table_template(request, lang, api_key)
--- app/framework/fastapi/types.py
+++ app/framework/fastapi/types.py
@@ -1,13 +1,14 @@
 from fastapi import Header, Query
 
 from app.core.common.patterns import API_KEY_PATTERN, TOKEN_PATTERN
-from app.core.common.types import Keywords
 from app.core.config.config import (
-    API_KEY_HEADER,
-    KEYWORDS_HEADER,
+    API_KEY_QUERY,
+    KEYWORDS_QUERY,
     TOKEN_HEADER,
 )
+from app.core.config.scopus import CURRENT_YEAR, LAST_DECADE
 from app.framework.fastapi.config import OPENAPI_EXAMPLE
+
 
 TokenHeader: str = Header(
     alias=TOKEN_HEADER,
@@ -19,16 +20,51 @@ TokenHeader: str = Header(
 )
 
 APIKeyQuery: str = Query(
-    alias=API_KEY_HEADER,
+    alias=API_KEY_QUERY,
     description="Your Scopus API Key",
     min_length=32,
     max_length=32,
     pattern=API_KEY_PATTERN,
 )
 
-KeywordsQuery: Keywords = Query(
-    alias=KEYWORDS_HEADER,
+KeywordsQuery: list[str] = Query(
+    alias=KEYWORDS_QUERY,
     description="Keywords to search for in the articles",
     min_length=1,
     max_length=4,
+)
+
+CountQuery: int = Query(
+    alias="count",
+    description="",
+    ge=1,
+    le=200,
+    decimal_places=None,
+)
+
+ThresholdQuery: int = Query(
+    # default=0,
+    alias="threshold",
+    description="",
+    ge=0,
+    le=100,
+    decimal_places=None,
+)
+
+StartYearQuery: int = Query(
+    # default=LAST_THREE_YEARS,
+    alias="Start Year",
+    description="",
+    ge=LAST_DECADE,
+    le=(CURRENT_YEAR - 1),
+    decimal_places=None,
+)
+
+EndYearQuery: int = Query(
+    # default=CURRENT_YEAR,
+    alias="Start Year",
+    description="",
+    ge=(LAST_DECADE + 1),
+    le=CURRENT_YEAR,
+    decimal_places=None,
 )
