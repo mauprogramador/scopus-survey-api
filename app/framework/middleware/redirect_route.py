d210eab5d7d24cdadbdc6e7f0eb60b22747d179b
mauprogramador <sir.silvabmauricio@gmail.com>
2024-12-27 17:25:21 -0400

    feat: update and add middlewares

--- app/framework/middleware/redirect_route.py
+++ app/framework/middleware/redirect_route.py
@@ -1,46 +1,63 @@
+from http import HTTPStatus
+from re import match
 from fastapi import FastAPI, Request
-from fastapi.responses import RedirectResponse
+from fastapi.responses import HTMLResponse
 from starlette.middleware.base import (
     BaseHTTPMiddleware,
     RequestResponseEndpoint,
 )
 from starlette.responses import Response
 
+from app.adapters.helpers.template_builder import TemplateBuilder
 from app.adapters.presenters.exception_json import ExceptionJSON
+from app.core.common.patterns import LANG_ROUTES, SURVEY_ROUTE
 from app.core.config.config import LOG
 from app.core.domain.exceptions import InterruptError
-from app.utils.signal_handler import SignalHandler
+from app.utils.signal_handler import ShutdownSignalHandler
 
 
 class RedirectNotFoundRoutes(BaseHTTPMiddleware):
     """Middleware to redirect any request for routes not found"""
 
-    __MAIN_ROUTE = "/scopus-survey/api"
-    __RELOAD_ROUTE = "/livereload"
-
     def __init__(self, app: FastAPI):
         """Middleware to redirect any request for routes not found"""
         super().__init__(app)
-        self.__handler = SignalHandler(True)
+        self.__handler = ShutdownSignalHandler(True)
 
     async def dispatch(
         self, request: Request, call_next: RequestResponseEndpoint
-    ) -> RedirectResponse | ExceptionJSON | Response:
+    ) -> HTMLResponse | ExceptionJSON | Response:
 
+        url = request.url.path
         response = await call_next(request)
-
-        is_main_route = request.url.path.count(self.__MAIN_ROUTE)
-        is_reload_route = request.url.path.count(self.__RELOAD_ROUTE)
-        allowed_route = bool(is_main_route) or bool(is_reload_route)
-
-        if response.status_code == 404 and not allowed_route:
-            LOG.error("Route Not Found")
-            return RedirectResponse(url=self.__MAIN_ROUTE)
 
         if self.__handler.event.is_set():
             error = InterruptError()
             LOG.exception(error)
 
-            return ExceptionJSON(request, error.code, error.message)
+            response = ExceptionJSON(request, error.code, error.message)
+
+        if response.status_code >= HTTPStatus.BAD_REQUEST:
+
+            message = "Unexpected error"
+
+            if response.status_code == HTTPStatus.NOT_FOUND and not match(
+                SURVEY_ROUTE, url
+            ):
+                message = "Resource Not Found"
+                LOG.error(message)
+
+            elif (
+                response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
+                and match(LANG_ROUTES, url)
+            ):
+                message = "Language Not Supported"
+                LOG.error(message)
+
+            LOG.trace(request, response.status_code, 0)
+
+            response = TemplateBuilder.not_found_template(
+                request, response, message
+            )
 
         return response
--- app/framework/middleware/tracing_exception.py
+++ app/framework/middleware/tracing_exception.py
@@ -1,4 +1,5 @@
-from time import time
+from http import HTTPStatus
+from time import perf_counter
 
 from fastapi import FastAPI, Request
 from starlette.middleware.base import (
@@ -11,44 +12,49 @@ from app.adapters.presenters.exception_json import ExceptionJSON
 from app.core.common.messages import UNEXPECTED_ERROR
 from app.core.config.config import LOG
 from app.core.domain.exceptions import InterruptError
-from app.utils.signal_handler import SignalHandler
+from app.utils.signal_handler import ShutdownSignalHandler
 
 
 class TraceExceptionControl(BaseHTTPMiddleware):
     """Middleware for handling request tracing and exceptions"""
 
+    __PROCESS_TIME = "X-Process-Time"
+
     def __init__(self, app: FastAPI):
         """Middleware for handling request tracing and exceptions"""
         super().__init__(app)
-        self.__handler = SignalHandler(True)
+        self.__handler = ShutdownSignalHandler(True)
 
     async def dispatch(
         self, request: Request, call_next: RequestResponseEndpoint
     ) -> ExceptionJSON | Response:
 
-        start_time = time()
+        start_time = perf_counter()
         try:
             response = await call_next(request)
 
         except InterruptError as exc:
-            LOG.trace(request, exc.code, start_time)
             LOG.exception(exc)
 
-            return ExceptionJSON(exc.code, exc.message, exc.errors)
+            response = ExceptionJSON(exc.code, exc.message, exc.errors)
 
         except Exception as exc:  # pylint: disable=W0718
-            LOG.trace(request, 500, start_time)
             LOG.exception(exc)
             message = UNEXPECTED_ERROR.format(repr(exc))
 
-            return ExceptionJSON(request, 500, message)
+            response = ExceptionJSON(
+                request, HTTPStatus.INTERNAL_SERVER_ERROR, message
+            )
 
         if self.__handler.event.is_set():
             error = InterruptError()
-            LOG.trace(request, error.code, start_time)
             LOG.exception(error)
 
-            return ExceptionJSON(request, error.code, error.message)
+            response = ExceptionJSON(request, error.code, error.message)
 
-        LOG.trace(request, response.status_code, start_time)
+        process_time = perf_counter() - start_time
+        response.headers[self.__PROCESS_TIME] = f"{process_time:.2f}s"
+
+        LOG.trace(request, response.status_code, process_time)
+
         return response
