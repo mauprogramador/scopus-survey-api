be93d83f3f8a4076818f433b97b9c466a6879e6f
mauprogramador <sir.silvabmauricio@gmail.com>
2025-01-17 17:08:45 -0400

    feat: update use cases

--- app/core/use_cases/scopus_articles_aggregator.py
+++ app/core/use_cases/scopus_articles_aggregator.py
@@ -4,32 +4,33 @@ from os.path import join
 from fastapi.responses import FileResponse
 from pandas import DataFrame
 
+from app.core.data.query import SearchParams
 from app.core.config.config import DIRECTORY, FILE, LOG
 from app.core.config.scopus import AUTHORS_COLUMN, TITLE_COLUMN
 from app.core.data.serializers import CSVFileHeaders
-from app.core.data.validators import SearchParams
-from app.core.domain.metaclasses import (
-    AbstractAPI,
-    ArticlesAggregator,
-    SearchAPI,
-    SimilarityFilter,
+from app.core.domain.interfaces import (
+    AbstractAPIABC,
+    ArticlesAggregatorABC,
+    SearchAPIABC,
+    SimilarityFilterABC,
 )
 
 
-class ScopusArticlesAggregator(ArticlesAggregator):
+class ScopusArticlesAggregator(ArticlesAggregatorABC):
     """Gathers, filters and compiles data from Scopus articles"""
 
     __DROP_COLUMNS = [TITLE_COLUMN, AUTHORS_COLUMN]
     __MEDIA_TYPE = "text/csv"
     __ROWS_INDEX = 0
+    __NON_RATIO = 0
     __PERCENT = 100
     __SEP = ";"
 
     def __init__(
         self,
-        search_api: SearchAPI,
-        abstract_api: AbstractAPI,
-        similarity_filter: SimilarityFilter,
+        search_api: SearchAPIABC,
+        abstract_api: AbstractAPIABC,
+        similarity_filter: SimilarityFilterABC,
     ) -> None:
         """Gathers, filters and compiles data from Scopus articles"""
         self.__search_api = search_api
@@ -37,7 +38,7 @@ class ScopusArticlesAggregator(ArticlesAggregator):
         self.__similarity_filter = similarity_filter
         self.__dataframe: DataFrame = None
 
-    def get_articles(self, params: SearchParams) -> FileResponse:
+    def retrieve_articles(self, params: SearchParams) -> FileResponse:
         entry_items = self.__search_api.search_articles(params)
         self.__dataframe = self.__abstract_api.retrieve_abstracts(
             params.api_key, entry_items
@@ -51,11 +52,15 @@ class ScopusArticlesAggregator(ArticlesAggregator):
         self.__dataframe = self.__dataframe.drop_duplicates(drop_subset)
         self.__dataframe = self.__dataframe.reset_index(drop=True)
 
-        self.__dataframe = self.__similarity_filter.filter(self.__dataframe)
+        if params.ratio != self.__NON_RATIO:
+            self.__dataframe = self.__similarity_filter.filter(
+                self.__dataframe
+            )
+
         result = rows_before - self.__dataframe.shape[self.__ROWS_INDEX]
         total_loss = (result / rows_before) * self.__PERCENT
 
-        LOG.info(f"Total articles loss: {total_loss:.2f}%")
+        LOG.info(f"Total articles loss: \033[33;1m{total_loss:.2f}%")
 
         filename = f"{params.api_key}_{FILE}"
         file_path = join(DIRECTORY, filename)
