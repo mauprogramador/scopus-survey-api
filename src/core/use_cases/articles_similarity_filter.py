879c27f4a5e31c9294cb7a34c4fdc93176fde64a
mauprogramador <sir.silvabmauricio@gmail.com>
2025-07-21 19:28:06 -0400

    refactor: use process pool to filter multiple groups

--- src/core/use_cases/articles_similarity_filter.py
+++ src/core/use_cases/articles_similarity_filter.py
@@ -1,20 +1,60 @@
+from concurrent.futures import (
+    CancelledError,
+    ProcessPoolExecutor,
+    as_completed,
+)
+from itertools import combinations
+from os import cpu_count, sched_getaffinity
+
 from pandas import DataFrame, to_datetime
 from thefuzz.fuzz import ratio  # type: ignore
 
+from src.core.common.messages import CANCELLED_ERROR
 from src.core.config.config import LOG
 from src.core.data.enums import Column
-from src.core.domain.interfaces import SimilarityFilterABC
 
 
-class ArticlesSimilarityFilter(SimilarityFilterABC):
+class ArticlesSimilarityFilter:
     """Filter articles from identical authors with similar titles"""
 
     __DATEFMT = "%Y-%m-%d"
+    __SIZE = 2
 
     def __init__(self) -> None:
         """Filter articles from identical authors with similar titles"""
+        self.__ratio: int = None
+
+        try:
+            self.__workers = min(len(sched_getaffinity(0)) - 1, 4)
+        except AttributeError:
+            cpu_cores = cpu_count()
+            self.__workers = cpu_cores - 1 if cpu_cores else 4
+
+    def __drop_singles(self, group: DataFrame) -> bool:
+        return group.shape[0] > 1
+
+    def __process_group(self, group: DataFrame):
+        title = group[Column.TITLE]
+
+        if title.shape[0] == 2:
+            if ratio(title.iloc[0], title.iloc[1]) > self.__ratio:
+                return int(group[Column.DATE].idxmax())
+
+            return None
+
+        rows_indexes: set[int] = set()
+
+        for indexes in combinations(range(group.shape[0]), self.__SIZE):
+            titles = title.iloc[indexes[0]], title.iloc[indexes[1]]
+
+            if ratio(titles[0], titles[1]) > self.__ratio:
+                rows_indexes.add(group.index[indexes[0]])
+                rows_indexes.add(group.index[indexes[1]])
+
+        return rows_indexes
 
     def filter(self, dataframe: DataFrame, similarity_ratio: int) -> DataFrame:
+        self.__ratio = similarity_ratio
         grouped_df = dataframe[Column.FILTER].groupby(Column.AUTHORS)
 
         LOG.debug({"same_authors_count": grouped_df.ngroups})
@@ -29,30 +69,44 @@ class ArticlesSimilarityFilter(SimilarityFilterABC):
         grouped_df = filtered_df.groupby(Column.AUTHORS)
         similar_titles: set[int] = set()
 
-        for _, group in grouped_df:
-            title = group[Column.TITLE]
+        max_workers = min(dataframe.shape[0], self.__workers)
+        LOG.debug({"max_workers": max_workers})
 
-            if title.shape[0] == 2:
-                if ratio(title.iloc[0], title.iloc[1]) > similarity_ratio:
-                    oldest_index = group[Column.DATE].idxmax()
-                    similar_titles.add(oldest_index)
-                continue
+        with ProcessPoolExecutor(max_workers) as executor:
+            all_tasks = {
+                executor.submit(self.__process_group, group)
+                for _, group in grouped_df
+            }
+            remaining_tasks = all_tasks.copy()
 
-            rows_indexes: set[int] = set()
+            for future in as_completed(all_tasks):
+                remaining_tasks.discard(future)
 
-            for outer_index in range(group.shape[0]):
-                for inner_index in range(outer_index + 1, group.shape[0]):
-                    titles = title.iloc[outer_index], title.iloc[inner_index]
+                try:
+                    rows_indexes = future.result()
+                    if rows_indexes is None:
+                        continue
 
-                    if ratio(titles[0], titles[1]) > similarity_ratio:
-                        rows_indexes.add(group.index[outer_index])
-                        rows_indexes.add(group.index[inner_index])
+                    if isinstance(rows_indexes, int):
+                        similar_titles.add(rows_indexes)
+                        continue
 
-            similar_titles_subset = filtered_df.iloc[list(rows_indexes)]
-            latest_index = similar_titles_subset[Column.DATE].idxmin()
+                    similar_titles_subset = filtered_df.iloc[
+                        list(rows_indexes)
+                    ]
+                    latest_index = similar_titles_subset[Column.DATE].idxmin()
 
-            rows_indexes.discard(latest_index)
-            similar_titles.update(rows_indexes)
+                    rows_indexes.discard(latest_index)
+                    similar_titles.update(rows_indexes)
+
+                except (CancelledError, Exception) as exc:
+                    LOG.error(CANCELLED_ERROR)
+
+                    for task in remaining_tasks:
+                        if not task.done():
+                            task.cancel()
+
+                    raise exc
 
         if not similar_titles:
             return dataframe
@@ -63,6 +117,3 @@ class ArticlesSimilarityFilter(SimilarityFilterABC):
         dataframe = dataframe.reset_index(drop=True)
 
         return dataframe
-
-    def __drop_singles(self, group: DataFrame) -> bool:
-        return group.shape[0] > 1
