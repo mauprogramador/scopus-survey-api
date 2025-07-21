11a3d7be83f65413aa0f1e79799a5429310ca73a
mauprogramador <sir.silvabmauricio@gmail.com>
2025-07-21 19:23:18 -0400

    refactor: add two separeted validations

--- src/adapters/helpers/scopus_response.py
+++ src/adapters/helpers/scopus_response.py
@@ -1,45 +1,42 @@
 from http import HTTPStatus
 from json import JSONDecodeError
+from typing import Type
 
-from pydantic import BaseModel, ValidationError
-from requests import Response
+from pydantic import ValidationError
 
 from src.core.common.messages import (
     QUOTA_EXCEEDED,
     RATE_LIMIT_EXCEEDED,
     VALIDATE_ERROR,
 )
+from src.core.common.types import ResponseBundle, ScopusModel
 from src.core.config.config import LOG
 from src.core.data.enums import ScopusCode
-from src.core.data.serializers import ScopusErrorResponse, ScopusQuotaRateLimit
-from src.core.domain.http_exceptions import (
-    BadGateway,
-    InternalError,
-    ScopusAPIError,
+from src.core.data.serializers import (
+    ScopusAbstract,
+    ScopusErrorResponse,
+    ScopusQuotaRateLimit,
+    ScopusSearch,
 )
-from src.core.domain.interfaces import ScopusResponseABC
+from src.core.domain.http_exceptions import InternalError, ScopusAPIError
 
 
-class ScopusResponse(ScopusResponseABC):
+class ScopusResponse:
     """Handle errors and status from Scopus responses"""
 
-    def __init__(self, model: BaseModel, exc_message: str) -> None:
-        """Handle errors and status from Scopus responses"""
-        self.__model = model
-        self.__exc_message = exc_message
-
-    def validate(self, response: Response) -> BaseModel:
-        code, json = response.status_code, response.json()
-        print(json)
-
+    @classmethod
+    def __validate(
+        cls, model: Type[ScopusModel], response: ResponseBundle
+    ) -> ScopusModel:
         try:
-            quota = ScopusQuotaRateLimit.model_validate(response.headers)
-            LOG.quota(quota, code)
+            if response.code >= HTTPStatus.BAD_REQUEST:
+                quota = ScopusQuotaRateLimit.model_validate(response.headers)
+                LOG.quota(quota, response.code)
 
-            if code != HTTPStatus.OK:
-
-                if code == HTTPStatus.TOO_MANY_REQUESTS:
-                    error_response = ScopusErrorResponse.model_validate(json)
+                if response.code == HTTPStatus.TOO_MANY_REQUESTS:
+                    error_response = ScopusErrorResponse.model_validate(
+                        response.data
+                    )
 
                     if error_response.code == ScopusCode.QUOTA:
                         LOG.error(QUOTA_EXCEEDED)
@@ -51,12 +48,19 @@ class ScopusResponse(ScopusResponseABC):
                     if error_response.code == ScopusCode.RATE_LIMIT:
                         LOG.error(RATE_LIMIT_EXCEEDED)
 
-                raise ScopusAPIError(code, json, quota.status)
+                raise ScopusAPIError(
+                    response.code, response.data, quota.status
+                )
 
-            if not response.text:
-                raise BadGateway(self.__exc_message)
-
-            return self.__model.model_validate(json)
+            return model.model_validate(response.data)
 
         except (ValidationError, JSONDecodeError, KeyError) as exc:
             raise InternalError(VALIDATE_ERROR, exc) from exc
+
+    @classmethod
+    def validate_search(cls, response: ResponseBundle) -> ScopusSearch:
+        return cls.__validate(ScopusSearch, response)
+
+    @classmethod
+    def validate_abstract(cls, response: ResponseBundle) -> ScopusAbstract:
+        return cls.__validate(ScopusAbstract, response)
