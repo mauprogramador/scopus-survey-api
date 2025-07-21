b08c1b9dde366a9bae0f57d8f0cfac5cf358470e
mauprogramador <sir.silvabmauricio@gmail.com>
2025-07-21 19:07:43 -0400

    feat: add new patterns

--- src/core/common/patterns.py
+++ src/core/common/patterns.py
@@ -4,11 +4,23 @@ HOST_PATTERN = r"^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$"
 # e.g. \033[35;1m, \033[m
 ANSI_ESCAPE_PATTERN = r"\x1b\[[0-9\;]*m"
 
+# e.g. SCOPUS_ID:84884919538
+SCOPUS_ID_PATTERN = r"^SCOPUS_ID\:[0-9]{10,}$"
+
+# e.g. http://api.elsevier.com/content/abstract/scopus_id/84884919538
+ABSTRACT_URL_PATTERN = (
+    r"^https\:\/\/api\.elsevier\.com\/content\/abstract"
+    r"\/scopus_id\/[0-9]{10,}$"
+)
+
 # e.g. 6bd9327547a3cf4c56586324df4b7d92
 API_KEY_PATTERN = r"^[a-zA-Z0-9]{32}$"
 
+# e.g. apiKey=6bd9327547a3cf4c56586324df4b7d92
+API_KEY_LOG_PATTERN = r"apiKey\=[a-zA-Z0-9]{32}"
+
 # e.g. Python, Multi-task
-KEYWORD_PATTERN = r"^[a-zA-Z0-9\-\_ ]{2,50}$"
+KEYWORD_PATTERN = r"^[a-zA-Z0-9\{\}\?\"\*\-\_ ]{2,50}$"
 
 # e.g. 989a5e2a50389ae6a5faf4c271d8bfb30cbbd88c
 TOKEN_PATTERN = r"^[a-zA-Z0-9]{40}$"
@@ -17,7 +29,7 @@ TOKEN_PATTERN = r"^[a-zA-Z0-9]{40}$"
 API_ROUTES_PATTERN = r"^\/v2\/scopus-survey\/api\/(combination|survey|csv)"
 
 # e.g. english, portuguese
-LANGUAGE_PATTERN = r"^[a-z\-\' ]{2,}$"
+LANGUAGE_PATTERN = r"^[a-z\-\' ]{3,30}$"
 
 # e.g. Python AND Multi-task
-COMBINATION_PATTERN = r"^[a-zA-Z0-9\-\_ ]{2,150}$"
+COMBINATION_PATTERN = r"^[a-zA-Z0-9\{\}\?\"\*\-\_ ]{9,215}$"
