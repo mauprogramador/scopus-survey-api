ef567f6f436bc80d15bbe6aa2f0261a417c499ad
mauprogramador <sir.silvabmauricio@gmail.com>
2025-07-21 19:14:49 -0400

    feat: use urllib in urlbuilder

--- src/adapters/helpers/url_builder.py
+++ src/adapters/helpers/url_builder.py
@@ -1,42 +1,103 @@
-from urllib.parse import quote_plus
+from urllib.parse import urlencode, urljoin
 
-from src.core.common.types import SearchParams
-from src.core.config.scopus import (
-    ABSTRACT_API_URL,
-    BOOLEAN_OPERATOR,
-    FIELDS,
-    PAGINATION_URL,
-    SEARCH_API_URL,
+from src.core.common.types import (
+    CombinationBundle,
+    CombinationParams,
+    Keyword,
+    SearchParams,
 )
-from src.core.domain.interfaces import URLBuilderABC
+from src.core.config.scopus import (
+    ARTICLE_PAGE_URL,
+    BOOLEAN_OPERATOR,
+    QUERY_FIELDS,
+    SEARCH_API_URL,
+    SEARCH_FIELDS,
+)
 
 
-class URLBuilder(URLBuilderABC):
+class URLBuilder:
     """Generate and format URLs for HTTP requests"""
+
+    __SEPARATOR = ","
+    __BASE_SEARCH_QUERY = {
+        "apiKey": None,
+        "query": None,
+        "field": "dc:identifier",
+        "suppressNavLinks": "true",
+        "date": None,
+        "start": "0",
+        "count": "1",
+        "sort": "+pubyear,+coverDate,+relevancy",
+    }
 
     def __init__(self) -> None:
         """Generate and format URLs for HTTP requests"""
-        self.__search_url: str = None
-        self.__api_key: str = None
+        self.__query: dict[str, str | None] = None
+        self.__search_terms: str = None
+
+    @staticmethod
+    def article_page_url(scopus_id: str) -> str:
+        query = {
+            "partnerID": "HzOxMe3b",
+            "scp": scopus_id,
+            "origin": "inward",
+        }
+        return urljoin(ARTICLE_PAGE_URL, f"?{urlencode(query)}")
+
+    def set_combination_query(self, params: CombinationParams) -> None:
+        query_fields = params.model_dump(
+            by_alias=True, exclude_unset=True, include=QUERY_FIELDS
+        )
+        query_terms = {"TITLE-ABS-KEY": "{combination}"}
+        query_terms.update(query_fields)
+
+        self.__search_terms = BOOLEAN_OPERATOR.join(
+            f"{field}({value})" for field, value in query_terms.items()
+        )
+
+        self.__query = self.__BASE_SEARCH_QUERY.copy()
+        self.__query["apiKey"] = params.api_key
+        self.__query["date"] = params.date
+        del self.__query["start"]
+
+    def combination_url(
+        self, arrangement: tuple[Keyword, ...]
+    ) -> CombinationBundle:
+        combination = BOOLEAN_OPERATOR.join(arrangement)
+
+        self.__query["query"] = self.__search_terms.format(
+            combination=combination
+        )
+        url = urljoin(SEARCH_API_URL, f"?{urlencode(self.__query)}")
+
+        return CombinationBundle(combination=combination, url=url)
 
     def search_url(self, params: SearchParams) -> str:
-        keywords = quote_plus(BOOLEAN_OPERATOR.join(params.keywords))
-        self.__search_url = SEARCH_API_URL.format(
-            apikey=params.api_key,
-            keywords=keywords,
-            date=params.year_range,
+        query_fields = params.model_dump(
+            by_alias=True, exclude_unset=True, include=QUERY_FIELDS
         )
-        return self.__search_url
+        query_terms = {"TITLE-ABS-KEY": params.combination}
+        query_terms.update(query_fields)
+
+        search_terms = BOOLEAN_OPERATOR.join(
+            f"{field}({value})" for field, value in query_terms.items()
+        )
+
+        self.__query = self.__BASE_SEARCH_QUERY.copy()
+        self.__query["apiKey"] = params.api_key
+        self.__query["query"] = search_terms
+        self.__query["date"] = params.date
+        del self.__query["count"]
+
+        return urljoin(SEARCH_API_URL, f"?{urlencode(self.__query)}")
 
     def pagination_url(self, page: int) -> str:
-        return PAGINATION_URL.format(search_url=self.__search_url, page=page)
+        self.__query["start"] = page
+        return urljoin(SEARCH_API_URL, f"?{urlencode(self.__query)}")
 
-    def set_abstract_api_key(self, api_key: str) -> None:
-        self.__api_key = api_key
+    def set_abstract_query(self, api_key: str) -> None:
+        search_fields = self.__SEPARATOR.join(SEARCH_FIELDS)
+        self.__query = {"apiKey": api_key, "field": search_fields}
 
     def abstract_url(self, abstract_url: str) -> str:
-        return ABSTRACT_API_URL.format(
-            abstract_url=abstract_url,
-            apikey=self.__api_key,
-            fields=quote_plus(FIELDS),
-        )
+        return urljoin(abstract_url, f"?{urlencode(self.__query)}")
