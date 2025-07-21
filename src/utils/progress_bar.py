c4b189dd6791982e4696320da973d169892ae049
mauprogramador <sir.silvabmauricio@gmail.com>
2025-07-21 19:36:16 -0400

    refactor: wrap logging redirect in progress bar

--- src/utils/progress_bar.py
+++ src/utils/progress_bar.py
@@ -1,4 +1,10 @@
+from contextlib import ExitStack
+from typing import Self
+
 from tqdm import tqdm
+from tqdm.contrib.logging import logging_redirect_tqdm
+
+from src.core.config.config import LOG
 
 
 class ProgressBar:
@@ -18,9 +24,10 @@ class ProgressBar:
         self, total: int, step: int = None, start: int = None
     ) -> None:
         """Display a progress bar for running tasks"""
-        self.__was_started = True
+        self.__stack = ExitStack()
         self.__total = total
         self.__step = 1 if step is None else step
+
         self.__progress_bar = tqdm(
             desc=self.__PREFIX,
             total=total,
@@ -29,15 +36,23 @@ class ProgressBar:
             position=self.__POSITION,
             colour=self.__COLOR,
         )
+
         if start is not None:
             self.__progress_bar.update(start * step)
 
     def step(self) -> None:
-        if self.__was_started:
-            progress = int(self.__progress_bar.n)
-            fits = (progress + self.__step) < self.__total
-            step = self.__step if fits else (self.__total - progress)
-            self.__progress_bar.update(step)
+        progress = int(self.__progress_bar.n)
+        fits = (progress + self.__step) < self.__total
+        step = self.__step if fits else (self.__total - progress)
+        self.__progress_bar.update(step)
 
     def close(self) -> None:
         self.__progress_bar.close()
+
+    def __enter__(self) -> Self:
+        self.__stack.enter_context(logging_redirect_tqdm(LOG.logger))
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.__progress_bar.close()
+        return self.__stack.__exit__(exc_type, exc_val, exc_tb)
