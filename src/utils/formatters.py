fde24060bb7ad5391144779d11f33553a6ca2dec
mauprogramador <sir.silvabmauricio@gmail.com>
2025-07-21 19:26:58 -0400

    refactor: move get error message to logging

--- src/utils/formatters.py
+++
@@ -1,4 +0,0 @@
-def get_error_message(exc: Exception, message: str = None) -> str:
-    if message is not None:
-        return message
-    return exc.args[0] if exc.args and exc.args[0] else repr(exc)
