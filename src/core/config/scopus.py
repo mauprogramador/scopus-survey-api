d21b090168b8a8ba38d14476f0612d6d5eabfd3c
mauprogramador <sir.silvabmauricio@gmail.com>
2025-07-25 21:50:41 -0400

    style: change order and move footnote

--- src/core/config/scopus.py
+++ src/core/config/scopus.py
@@ -11,6 +11,15 @@ NULL = "null"
 NO_RESULTS = "NO_SEARCH_RESULTS"
 EMPTY_RESULT = "Result set was empty"
 
+SEARCH_API_URL = "https://api.elsevier.com/content/search/scopus"
+ARTICLE_PAGE_URL = "https://www.scopus.com/inward/record.uri"
+
+SCOPUS_DOCS = "https://dev.elsevier.com/documentation"
+FOOTNOTE = (
+    "# The data was retrieved from Scopus APIs on {date} via "
+    "http://api.elsevier.com and http://www.scopus.com."
+)
+
 QUERY_FIELDS = {
     "doctype",
     "pubstage",
@@ -20,10 +29,6 @@ QUERY_FIELDS = {
     "subject_area",
     "pages",
 }
-
-SEARCH_API_URL = "https://api.elsevier.com/content/search/scopus"
-ARTICLE_PAGE_URL = "https://www.scopus.com/inward/record.uri"
-SCOPUS_DOCS = "https://dev.elsevier.com/documentation"
 
 SEARCH_FIELDS = (
     "dc:identifier",
@@ -39,6 +44,19 @@ SEARCH_FIELDS = (
     "dc:creator",
     "authors",
 )
+
+SCOPUS_HEADERS = {
+    "Accept-Language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
+    "Accept-Charset": "ISO-8859-1,utf-8;q=0.7,*;q=0.3",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Referer": "https://www.scopus.com/",
+    "Origin": "https://www.scopus.com",
+    "Content-Type": "application/json;charset=UTF-8",
+    "Accept": "application/json",
+    "User-Agent": "Mozilla/5.0",
+    "Cache-Control": "no-cache",
+    "Connection": "keep-alive",
+}
 
 SCOPUS_ERRORS = {
     HTTPStatus.BAD_REQUEST: "Invalid Request: invalid information submitted",
@@ -61,17 +79,4 @@ SCOPUS_ERRORS = {
     HTTPStatus.INTERNAL_SERVER_ERROR: (
         "Generic Error: Scopus API back-end processing errors"
     ),
-}
-
-SCOPUS_HEADERS = {
-    "Accept-Language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
-    "Accept-Charset": "ISO-8859-1,utf-8;q=0.7,*;q=0.3",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Referer": "https://www.scopus.com/",
-    "Origin": "https://www.scopus.com",
-    "Content-Type": "application/json;charset=UTF-8",
-    "Accept": "application/json",
-    "User-Agent": "Mozilla/5.0",
-    "Cache-Control": "no-cache",
-    "Connection": "keep-alive",
 }
