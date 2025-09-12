80c8b0745147b0e01a2b83c8a49b6ee60ef867d1
mauprogramador <sir.silvabmauricio@gmail.com>
2025-09-12 01:52:16 -0400

    fix: use different http client instances for unique sessions

--- src/core/domain/factory.py
+++ src/core/domain/factory.py
@@ -33,12 +33,15 @@ def make_aggregator() -> ScopusArticlesAggregator:
     survey_detail = SurveyDetail()
 
     url_builder = URLBuilder()
-    http_client = HTTPClient()
+    search_http_client = HTTPClient()
+    abstract_http_client = HTTPClient()
 
-    search_api = ScopusSearchAPI(http_client, url_builder, survey_detail)
+    search_api = ScopusSearchAPI(
+        search_http_client, url_builder, survey_detail
+    )
 
     abstract_api = ScopusAbstractRetrievalAPI(
-        http_client, url_builder, survey_detail
+        abstract_http_client, url_builder, survey_detail
     )
 
     similarity_filter = ArticlesSimilarityFilter()
