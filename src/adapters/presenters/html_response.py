b74c8864e79503e1c0e601621e02973df081f76a
mauprogramador <sir.silvabmauricio@gmail.com>
2025-09-12 01:41:13 -0400

    feat: pass current year in template context

--- src/adapters/presenters/html_response.py
+++ src/adapters/presenters/html_response.py
@@ -9,8 +9,8 @@ from starlette.responses import Response
 from src import __version__
 from src.adapters.presenters.error_response import ErrorJSON
 from src.core.common.error_messages import UNEXPECTED_ERROR
-from src.core.common.types import Translation
-from src.core.config.config import LOG, MAX_AGE, PREFIX
+from src.core.config.config import MAX_AGE, PREFIX
+from src.core.config.scopus import CURRENT_YEAR
 from src.core.data.enums import Lang, Templates
 from src.core.domain.translations import Translations
 
