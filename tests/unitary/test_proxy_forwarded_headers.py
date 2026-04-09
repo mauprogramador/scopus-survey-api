0c967e5257a97d1824532bbcc51d22f6bf7f2c04
mauprogramador <sir.silvabmauricio@gmail.com>
2026-04-09 17:47:22 -0400

    test: add proxy forwarded headers middleware unit test

---
+++ tests/unitary/test_proxy_forwarded_headers.py
@@ -0,0 +1,70 @@
+# mypy: disable-error-code="index"
+from unittest.mock import AsyncMock
+
+import fastapi
+from pytest import fixture, mark
+from starlette.testclient import TestClient
+from starlette.types import Scope
+
+from src.framework.middleware.proxy_forwarded_headers import (
+    ProxyForwardedHeadersMiddleware,
+)
+from tests.mocks.raw import HTTP_200
+
+
+@mark.asyncio
+async def test_success_headers():
+    scope: Scope = {  # type: ignore
+        "type": "http",
+        "method": "GET",
+        "scheme": "http",
+        "server": ("127.0.0.1", 8000),
+        "headers": [
+            (b"host", b"localhost:8000"),
+            (b"x-forwarded-proto", b"https"),
+            (b"x-forwarded-host", b"any.com.br"),
+        ],
+    }
+
+    request = fastapi.Request(scope)
+    call_next = AsyncMock()
+    middleware = ProxyForwardedHeadersMiddleware(None)
+
+    await middleware.dispatch(request, call_next)
+    call_next.assert_called_once_with(request)
+
+    assert request.scope["scheme"] == "https"
+    assert request.scope["server"][0] == "any.com.br"
+    assert request.scope["headers"][0] == (b"host", b"any.com.br")
+
+
+@fixture(scope="module", name="client")
+def sub_app_test_client():
+    app = fastapi.FastAPI()
+    app.add_middleware(ProxyForwardedHeadersMiddleware)
+
+    @app.get("/test")
+    def test_url_for_route(request: fastapi.Request):
+        return {"url": str(request.url_for("test_url_for_route"))}
+
+    client = TestClient(app)
+    return client
+
+
+def test_proxy_headers_updates_url_for(client: TestClient):
+
+    headers = {
+        "X-Forwarded-Proto": "https",
+        "X-Forwarded-Host": "any.com.br",
+    }
+
+    res = client.get("/test", headers=headers)
+    assert res.status_code == HTTP_200
+    assert res.json()["url"] == "https://any.com.br/test"
+
+
+def test_no_proxy_headers_uses_default(client: TestClient):
+    res = client.get("/test")
+
+    assert res.status_code == HTTP_200
+    assert res.json()["url"].startswith("http://testserver")
