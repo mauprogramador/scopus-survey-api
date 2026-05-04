948f3a39d3bcb90968e8f6f86b447e8bf0486b64
mauprogramador <sir.silvabmauricio@gmail.com>
2026-05-04 19:17:38 -0400

    test: check default headers, rename and fix internal error test

--- tests/integration/test_flow_guarding_monitor.py
+++ tests/integration/test_flow_guarding_monitor.py
@@ -7,7 +7,8 @@ from starlette.middleware.base import _StreamingResponse
 from src.adapters.presenters.csv_response import CSVResponse
 from src.adapters.presenters.json_response import ErrorJSON
 from src.adapters.presenters.template_response import TemplateResponse
-from src.core.config.config import RATELIMIT_POLICY, SERVER
+from src.core.config.config import HEADERS, RATELIMIT_POLICY, SERVER
+from src.framework.fastapi.csrf_token import CSRFToken
 from tests.conftest import assert_error_json
 from tests.mocks.helpers import fqn
 from tests.mocks.raw import (
@@ -22,6 +23,7 @@ from tests.mocks.raw import (
 
 
 RETRIEVE = fqn(CSVResponse.retrieve)
+GENERATE_TOKEN = fqn(CSRFToken.generate_csrf_tokens)
 
 
 @mark.asyncio
@@ -32,6 +34,8 @@ async def test_success_headers(client: Client):
     assert res.status_code == HTTP_200
     assert res.headers["X-Trace-ID"] and res.headers["X-Process-Time"]
     assert res.headers["Content-Security-Policy"] is not None
+    headers = {key.lower(): value for key, value in HEADERS.items()}
+    assert headers.items() <= dict(res.headers).items()
     assert res.headers["X-RateLimit-Policy"] == RATELIMIT_POLICY
     assert res.headers["Server"] == SERVER.get()
 
@@ -46,7 +50,7 @@ async def test_uncaught_exception(mocker: Mocker, client: Client):
 
 
 @mark.asyncio
-async def test_not_found_status(mocker: Mocker, client: Client):
+async def test_routing_error(mocker: Mocker, client: Client):
     client.cookies.clear()
     client.headers.clear()
     spy = mocker.spy(TemplateResponse, "not_found_template")
@@ -57,11 +61,12 @@ async def test_not_found_status(mocker: Mocker, client: Client):
 
 
 @mark.asyncio
-async def test_not_found_error(mocker: Mocker, client: Client):
+async def test_internal_error(mocker: Mocker, client: Client):
     client.cookies.clear()
     client.headers.clear()
+    mocker.patch(GENERATE_TOKEN, side_effect=RuntimeError("any"))
     spy = mocker.spy(TemplateResponse, "not_found_template")
-    res = await client.get("/api/any")
-    assert res.status_code == HTTP_404 and res.text
+    res = await client.get(URL_WEB)
+    assert res.status_code == HTTP_500 and res.text
     assert res.headers["Content-Type"] == HTML_CONTENT_TYPE
     isinstance(spy.call_args_list[0].args[1], ErrorJSON)
