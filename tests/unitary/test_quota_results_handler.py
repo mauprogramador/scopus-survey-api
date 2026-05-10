5541ad8ddafade7f5894020a5fe0c5278b009822
mauprogramador <sir.silvabmauricio@gmail.com>
2026-05-10 18:20:32 -0400

    test: update all state tests for new method and to handle quota

--- tests/unitary/test_quota_results_handler.py
+++ tests/unitary/test_quota_results_handler.py
@@ -1,102 +1,147 @@
 from pytest import raises
 
-from src.core.common.error_messages import QUOTA_EXCEEDED
+from src.core.common.error_messages import (
+    ARTICLES_NOT_FOUND,
+    DATA_MISMATCH_ERROR,
+)
 from src.core.data.quota_results_handler import QuotaResultsHandler
 from src.core.data.serializers import ScopusSearch
-from src.core.domain.http_exceptions import TooManyRequests
+from src.core.domain.http_exceptions import (
+    BadGateway,
+    NotFound,
+)
 from tests.conftest import assert_http_error
 from tests.mocks.helpers import search_raw
-from tests.mocks.raw import HTTP_429, LOG_NO_QUOTA, LOG_ONE_QUOTA, LOG_QUOTA
+from tests.mocks.raw import (
+    HTTP_404,
+    HTTP_502,
+    LOG_NO_QUOTA,
+    LOG_ONE_QUOTA,
+    LOG_QUOTA,
+    RAW_SEARCH_NOT_FOUND,
+)
+
+
+STATE = QuotaResultsHandler()
 
 
 def test_search_valid_data():
-    first_search = ScopusSearch(**search_raw(7))
-    model = QuotaResultsHandler(first_search)
+    first_search = ScopusSearch(**search_raw(7, 1))
+    STATE.set_first_search(first_search)
 
-    assert model.total_results == 7 and model.items_per_page == 7
-    assert len(model.entry) == 7 and model.pages_count == 1
-    assert len(model.abstracts) == 0 and model.total_abstracts == 7
-
-
-def test_search_quota_ok():
-    first_search = ScopusSearch(**search_raw(156))
-    model = QuotaResultsHandler(first_search)
-
-    assert model.total_results == 156 and model.items_per_page == 25
-    assert len(model.entry) == 25 and model.pages_count == 7
-    assert len(model.abstracts) == 0 and model.total_abstracts == 156
-
-    model.handle_search_quota(LOG_QUOTA)
-    assert model.total_results == 156 and model.items_per_page == 25
-    assert len(model.entry) == 25 and model.pages_count == 7
-    assert len(model.abstracts) == 0 and model.total_abstracts == 156
+    assert STATE.total_results == 7 and STATE.items_per_page == 7
+    assert len(STATE.entry) == 1 and STATE.pages_count == 1
+    assert not STATE.total_abstracts and not STATE.abstracts
 
 
-def test_search_one_last_quota():
-    first_search = ScopusSearch(**search_raw(156))
-    model = QuotaResultsHandler(first_search)
+def test_search_not_found():
+    first_search = ScopusSearch(**RAW_SEARCH_NOT_FOUND)
+    with raises(NotFound) as info:
+        STATE.set_first_search(first_search)
+    assert_http_error(info, HTTP_404, ARTICLES_NOT_FOUND)
 
-    assert model.total_results == 156 and model.items_per_page == 25
-    assert len(model.entry) == 25 and model.pages_count == 7
-    assert len(model.abstracts) == 0 and model.total_abstracts == 156
 
-    model.handle_search_quota(LOG_ONE_QUOTA)
-    assert model.total_results == 50 and model.items_per_page == 25
-    assert len(model.entry) == 25 and model.pages_count == 2
-    assert len(model.abstracts) == 0 and model.total_abstracts == 50
+def test_validate_integrity():
+    first_search = ScopusSearch(**search_raw(1))
+    STATE.set_first_search(first_search)
+    STATE.validate_integrity()
+
+
+def test_validate_integrity_error():
+    first_search = ScopusSearch(**search_raw(156, 1))
+    STATE.set_first_search(first_search)
+    with raises(BadGateway) as info:
+        STATE.validate_integrity()
+    assert_http_error(info, HTTP_502, DATA_MISMATCH_ERROR)
+
+
+def test_fix_total():
+    first_search = ScopusSearch(**search_raw(156, 1))
+    STATE.set_first_search(first_search)
+    STATE.fix_total()
+
+    assert STATE.total_abstracts == 1 and len(STATE.abstracts) == 0
+
+
+def test_search_quota_greater():
+    first_search = ScopusSearch(**search_raw(156, 1))
+    STATE.set_first_search(first_search)
+
+    assert STATE.total_results == 156 and STATE.items_per_page == 25
+    assert len(STATE.entry) == 1 and STATE.pages_count == 7
+
+    STATE.handle_search_quota(LOG_QUOTA)
+    assert STATE.total_results == 156 and STATE.items_per_page == 25
+    assert len(STATE.entry) == 1 and STATE.pages_count == 7
+
+    assert STATE.pages_to_fetch == 6
+    assert list(STATE.pages_to_fetch_range) == [1, 2, 3, 4, 5, 6]
+    assert STATE.pages_to_fetch_progress == (156, 25, 1)
+
+
+def test_search_quota_lesses():
+    first_search = ScopusSearch(**search_raw(156, 1))
+    STATE.set_first_search(first_search)
+
+    assert STATE.total_results == 156 and STATE.items_per_page == 25
+    assert len(STATE.entry) == 1 and STATE.pages_count == 7
+
+    STATE.handle_search_quota(LOG_ONE_QUOTA)
+    assert STATE.total_results == 26 and STATE.items_per_page == 25
+    assert len(STATE.entry) == 1 and STATE.pages_count == 2
+    assert STATE.pages_to_fetch == 1
 
 
 def test_search_quota_exceeded():
-    first_search = ScopusSearch(**search_raw(1))
-    model = QuotaResultsHandler(first_search)
+    first_search = ScopusSearch(**search_raw(156, 1))
+    STATE.set_first_search(first_search)
 
-    assert model.total_results == 1 and model.items_per_page == 1
-    assert len(model.entry) == 1 and model.pages_count == 1
-    assert len(model.abstracts) == 0 and model.total_abstracts == 1
+    assert STATE.total_results == 156 and STATE.items_per_page == 25
+    assert len(STATE.entry) == 1 and STATE.pages_count == 7
 
-    with raises(TooManyRequests) as info:
-        model.handle_search_quota(LOG_NO_QUOTA)
-    assert_http_error(info, HTTP_429, QUOTA_EXCEEDED)
-
-
-def test_abstract_quota_ok():
-    first_search = ScopusSearch(**search_raw(156))
-    model = QuotaResultsHandler(first_search)
-
-    assert model.total_results == 156 and model.items_per_page == 25
-    assert len(model.entry) == 25 and model.pages_count == 7
-    assert len(model.abstracts) == 0 and model.total_abstracts == 156
-    first_entry_item_id = id(model.entry[0])
-
-    model.handle_abstract_quota(LOG_QUOTA)
-    assert model.total_results == 156 and model.items_per_page == 25
-    assert len(model.entry) == 24 and model.pages_count == 7
-    assert len(model.abstracts) == 0 and model.total_abstracts == 156
-    assert first_entry_item_id != id(model.entry[0])
+    STATE.handle_search_quota(LOG_NO_QUOTA)
+    assert STATE.total_results == 1 and STATE.items_per_page == 25
+    assert len(STATE.entry) == 1 and STATE.pages_count == 1
+    assert STATE.pages_to_fetch == 0
 
 
-def test_abstract_one_last_quota():
-    first_search = ScopusSearch(**search_raw(156))
-    model = QuotaResultsHandler(first_search)
+def test_abstract_quota_greater():
+    first_search = ScopusSearch(**search_raw(156, 7))
+    STATE.set_first_search(first_search)
+    STATE.fix_total()
 
-    assert model.total_results == 156 and model.items_per_page == 25
-    assert len(model.entry) == 25 and model.pages_count == 7
-    assert len(model.abstracts) == 0 and model.total_abstracts == 156
-    first_entry_item_id = id(model.entry[0])
+    assert STATE.total_abstracts == 7 and len(STATE.abstracts) == 0
+    assert len(STATE.entry) == 7
 
-    model.handle_abstract_quota(LOG_ONE_QUOTA)
-    assert model.total_abstracts == 2 and len(model.entry) == 25
-    assert first_entry_item_id == id(model.entry[0])
+    STATE.handle_abstract_quota(LOG_QUOTA)
+    assert STATE.total_abstracts == 7 and len(STATE.abstracts) == 0
+    assert len(STATE.entry) == 7
+
+    assert STATE.abstracts_to_fetch == 6
+    assert list(STATE.pages_to_fetch_range) == [1, 2, 3, 4, 5, 6]
+
+
+def test_abstract_quota_lesser():
+    first_search = ScopusSearch(**search_raw(156, 7))
+    STATE.set_first_search(first_search)
+    STATE.fix_total()
+
+    assert STATE.total_abstracts == 7 and len(STATE.abstracts) == 0
+    assert len(STATE.entry) == 7
+
+    STATE.handle_abstract_quota(LOG_ONE_QUOTA)
+    assert STATE.total_abstracts == 2 and len(STATE.abstracts) == 0
+    assert len(STATE.entry) == 7 and STATE.abstracts_to_fetch == 1
 
 
 def test_abstract_quota_exceeded():
-    first_search = ScopusSearch(**search_raw(1))
-    model = QuotaResultsHandler(first_search)
+    first_search = ScopusSearch(**search_raw(156, 7))
+    STATE.set_first_search(first_search)
+    STATE.fix_total()
 
-    assert model.total_results == 1 and model.items_per_page == 1
-    assert len(model.entry) == 1 and model.pages_count == 1
-    assert len(model.abstracts) == 0 and model.total_abstracts == 1
+    assert STATE.total_abstracts == 7 and len(STATE.abstracts) == 0
+    assert len(STATE.entry) == 7
 
-    with raises(TooManyRequests) as info:
-        model.handle_abstract_quota(LOG_NO_QUOTA)
-    assert_http_error(info, HTTP_429, QUOTA_EXCEEDED)
+    STATE.handle_abstract_quota(LOG_NO_QUOTA)
+    assert STATE.total_abstracts == 1 and len(STATE.abstracts) == 0
+    assert len(STATE.entry) == 7 and STATE.abstracts_to_fetch == 0
