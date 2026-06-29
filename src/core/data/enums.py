35e5f03d955fc9f1d4cf5cce7fa1a586cbf41337
mauprogramador <sir.silvabmauricio@gmail.com>
2026-06-29 17:05:34 -0400

    refactor: update error translations

--- src/core/data/enums.py
+++ src/core/data/enums.py
@@ -31,7 +31,7 @@ class ExcMsg(StrEnum):
     SCOPUS_API_ERROR = "Scopus API error"
     DATA_MISMATCH_ERROR = "Data sum mismatch in response"
     # Application errors
-    CANCELLED_ERROR = "Unexpected cancellation of requests"
+    CANCELLED_ERROR = "Unexpected cancellation of tasks"
     INTERNAL_ERROR = "Unexpected internal error occurred"
     SERIALIZE_ERROR = "Error serializing error details"
     ARTICLES_NOT_FOUND = "No articles found"
