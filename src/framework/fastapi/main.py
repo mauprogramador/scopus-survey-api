99f728165f0400ee722c24e681f2bcddf3237429
mauprogramador <sir.silvabmauricio@gmail.com>
2026-06-29 16:22:51 -0400

    fix: disable cors credentials

--- src/framework/fastapi/main.py
+++ src/framework/fastapi/main.py
@@ -64,7 +64,7 @@ app.add_middleware(FlowGuardingMonitorMiddleware)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["GET"],
     allow_headers=["*"],
 )
