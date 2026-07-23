2f403b5fb2efc17a35ef4b8c4756d981bb877669
mauprogramador <sir.silvabmauricio@gmail.com>
2026-07-23 10:27:04 -0400

    feat: add context object to replace state

---
+++ src/adapters/helpers/context.py
@@ -0,0 +1,26 @@
+import math
+from dataclasses import dataclass, field
+
+from src.core.common.types import Headers, Json, ScopusEntry, ScopusPage
+
+
+@dataclass
+class ScopusContext:
+    search_result: ScopusPage = None
+    total_results: int = None
+    search_headers: Headers = None
+    abstract_headers: Headers = None
+    entry: list[ScopusEntry] = field(default_factory=list)
+    abstracts: list[Json] = field(default_factory=list)
+
+    @property
+    def items_per_page(self) -> int:
+        if self.search_result:
+            return self.search_result.items_per_page
+        return 0
+
+    @property
+    def pages_count(self) -> int:
+        if self.total_results is None:
+            return 0
+        return math.ceil(self.total_results / self.items_per_page)
---
+++ tests/unitary/test_context.py
@@ -0,0 +1,30 @@
+from unittest.mock import ANY
+
+from src.adapters.helpers.context import ScopusContext
+from src.core.data.serializers import ScopusPage
+from tests.mocks.raw import RAW_SEARCH_OK
+
+
+_SEARCH_RESULT = ScopusPage(**RAW_SEARCH_OK)
+
+
+def test_survey_context():
+    ctx = ScopusContext()
+    assert not ctx.search_result
+    assert not ctx.total_results
+    assert not ctx.search_headers
+    assert not ctx.abstract_headers
+    assert not ctx.entry
+    assert not ctx.abstracts
+    assert ctx.items_per_page == 0
+    assert ctx.pages_count == 0
+
+    ctx.search_result = _SEARCH_RESULT
+    ctx.total_results = _SEARCH_RESULT.total_results
+    ctx.entry.append(ANY)
+    ctx.abstracts.append(ANY)
+
+    assert id(ctx.search_result) == id(_SEARCH_RESULT)
+    assert ctx.items_per_page == _SEARCH_RESULT.items_per_page == 1
+    assert ctx.pages_count == 1
+    assert ctx.entry and ctx.abstracts
