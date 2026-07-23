1021b2a5800acb3d5f13d71db6cc44df7fca3224
mauprogramador <sir.silvabmauricio@gmail.com>
2026-07-23 18:52:45 -0400

    test: refactor

--- tests/unitary/test_survey_orchestrator.py
+++ tests/unitary/test_survey_orchestrator.py
@@ -39,72 +39,61 @@ def _fixt(raw_dataset: list[Json]) -> tuple[SurveyAggregator, Mock]:
 
 @mark.asyncio
 async def test_one_row(mocker: Mocker):
-    fix = aggregator_fix(DIFFERENT_ARTICLES.iloc[0:1])
+    use_case, mock_filter = _fixt(DIFFERENT_ARTICLES[0:1])
     spy_drop = mocker.spy(DataFrame, "drop_duplicates")
-    spy_write = mocker.patch(WRITE_CSV, wraps=write_csv_file)
+    dataset, _ = await use_case.process(PARAMS)
 
-    res = await fix.use_case.retrieve_articles(PARAMS)
-    df_spy: DataFrame = spy_write.call_args_list[0].args[0]
-
-    assert res.status_code == HTTP_200 and df_spy.shape == (1, 11)
+    assert dataset.shape[0] == len(DIFFERENT_ARTICLES[0:1]) == 1
     spy_drop.assert_not_called()
-    fix.filter.assert_not_called()
+    mock_filter.assert_not_called()
 
 
 @mark.asyncio
 async def test_more_rows(mocker: Mocker):
-    fix = aggregator_fix(DIFFERENT_ARTICLES)
+    use_case, mock_filter = _fixt(DIFFERENT_ARTICLES)
     spy_drop = mocker.spy(DataFrame, "drop_duplicates")
-    spy_write = mocker.patch(WRITE_CSV, wraps=write_csv_file)
+    dataset, _ = await use_case.process(PARAMS)
 
-    res = await fix.use_case.retrieve_articles(PARAMS)
-    df_spy: DataFrame = spy_write.call_args_list[0].args[0]
-
-    assert res.status_code == HTTP_200 and df_spy.shape == (7, 11)
+    assert dataset.shape[0] == len(DIFFERENT_ARTICLES) == 7
     spy_drop.assert_called()
-    fix.filter.assert_called()
+    mock_filter.assert_called()
 
 
 @mark.asyncio
 async def test_drop_exact_duplicates(mocker: Mocker):
-    fix = aggregator_fix(EXACT_DUPLICATES)
+    use_case, mock_filter = _fixt(EXACT_DUPLICATES)
     spy_drop = mocker.spy(DataFrame, "drop_duplicates")
     spy_reset = mocker.spy(DataFrame, "reset_index")
 
-    res = await fix.use_case.retrieve_articles(PARAMS)
+    dataset, _ = await use_case.process(PARAMS)
     df_in: DataFrame = spy_drop.call_args_list[0].args[0]
     df_out: DataFrame = spy_reset.call_args_list[0].args[0]
 
-    assert res.status_code == HTTP_200
+    assert dataset.shape[0] == 1 and len(EXACT_DUPLICATES) == 2
     assert df_in.shape[0] == 2 and df_out.shape[0] == 1
-
-    fix.filter.assert_not_called()
+    mock_filter.assert_not_called()
 
 
 @mark.asyncio
 async def test_drop_same_title_and_authors(mocker: Mocker):
-    fix = aggregator_fix(SAME_TITLE_AND_AUTHORS)
+    use_case, mock_filter = _fixt(SAME_TITLE_AND_AUTHORS)
     spy_drop = mocker.spy(DataFrame, "drop_duplicates")
     spy_reset = mocker.spy(DataFrame, "reset_index")
 
-    res = await fix.use_case.retrieve_articles(PARAMS)
+    dataset, _ = await use_case.process(PARAMS)
     df_in: DataFrame = spy_drop.call_args_list[1].args[0]
     df_out: DataFrame = spy_reset.call_args_list[1].args[0]
 
-    assert res.status_code == HTTP_200
+    assert dataset.shape[0] == 1 and len(SAME_TITLE_AND_AUTHORS) == 2
     assert df_in.shape[0] == 2 and df_out.shape[0] == 1
-
-    fix.filter.assert_not_called()
+    mock_filter.assert_not_called()
 
 
 @mark.asyncio
-async def test_non_ratio(mocker: Mocker):
-    fix = aggregator_fix(DIFFERENT_ARTICLES)
+async def test_non_ratio():
+    use_case, mock_filter = _fixt(DIFFERENT_ARTICLES)
     PARAMS.ratio = 0
+    dataset, _ = await use_case.process(PARAMS)
 
-    spy = mocker.patch(WRITE_CSV, wraps=write_csv_file)
-    res = await fix.use_case.retrieve_articles(PARAMS)
-    df_spy: DataFrame = spy.call_args_list[0].args[0]
-
-    assert res.status_code == HTTP_200 and df_spy.shape == (7, 11)
-    fix.filter.assert_not_called()
+    assert dataset.shape[0] == len(DIFFERENT_ARTICLES) == 7
+    mock_filter.assert_not_called()
