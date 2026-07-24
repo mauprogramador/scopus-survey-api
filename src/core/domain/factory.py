4345358b8063893f9071f37b5854b4b9c1027dfa
mauprogramador <sir.silvabmauricio@gmail.com>
2026-07-23 21:20:16 -0400

    refactor: update factory dependencies

--- src/core/domain/factory.py
+++ src/core/domain/factory.py
@@ -1,54 +1,23 @@
-from src.adapters.gateway.scopus_abstract_retrieval_api import (
-    ScopusAbstractRetrievalAPI,
-)
-from src.adapters.gateway.scopus_search_api import ScopusSearchAPI
+from src.adapters.gateway.scopus_dataset_gatherer import ScopusDatasetGatherer
+from src.adapters.gateway.scopus_volume_scouter import ScopusVolumeScouter
 from src.adapters.helpers.http_client import HTTPClient
-from src.adapters.helpers.url_builder import URLBuilder
-from src.core.data.survey_details import SurveyDetails
-from src.core.data.survey_state import SurveyState
 from src.core.use_cases import (
     SimilarityFilter,
-    SurveyOrchestrator,
+    SurveyAggregator,
 )
-from src.core.use_cases.keyword_scouter import KeywordsScouter
+from src.core.use_cases.survey_combinations import SurveyCombinations
 
 
-def make_combinator() -> KeywordsScouter:
-    survey_details = SurveyDetails()
-
-    url_builder = URLBuilder()
+def make_combinator() -> SurveyCombinations:
     http_client = HTTPClient()
+    volume_scouter = ScopusVolumeScouter(http_client)
 
-    search_api = ScopusSearchAPI(
-        http_client, url_builder, survey_details, None
-    )
-
-    combinator_finder = KeywordsScouter(
-        url_builder, search_api, survey_details
-    )
-
-    return combinator_finder
+    return SurveyCombinations(volume_scouter)
 
 
-def make_aggregator() -> SurveyOrchestrator:
-    survey_details = SurveyDetails()
-    state = SurveyState()
-
-    url_builder = URLBuilder()
+def make_aggregator() -> SurveyAggregator:
     http_client = HTTPClient()
-
-    search_api = ScopusSearchAPI(
-        http_client, url_builder, survey_details, state
-    )
-
-    abstract_api = ScopusAbstractRetrievalAPI(
-        http_client, url_builder, survey_details, state
-    )
-
+    dataset_gatherer = ScopusDatasetGatherer(http_client)
     similarity_filter = SimilarityFilter()
 
-    articles_aggregator = SurveyOrchestrator(
-        search_api, abstract_api, similarity_filter, survey_details
-    )
-
-    return articles_aggregator
+    return SurveyAggregator(dataset_gatherer, similarity_filter)
--- tests/unitary/test_factory.py
+++ tests/unitary/test_factory.py
@@ -1,85 +1,45 @@
 from pytest import mark
 
-from src.adapters.gateway.scopus_abstract_retrieval_api import (
-    ScopusAbstractRetrievalAPI,
-)
-from src.adapters.gateway.scopus_search_api import ScopusSearchAPI
+from src.adapters.gateway.scopus_dataset_gatherer import ScopusDatasetGatherer
+from src.adapters.gateway.scopus_volume_scouter import ScopusVolumeScouter
+from src.adapters.helpers.context import ScopusContext
 from src.adapters.helpers.http_client import HTTPClient
-from src.adapters.helpers.url_builder import URLBuilder
-from src.core.data.survey_details import SurveyDetails
-from src.core.data.survey_state import SurveyState
 from src.core.domain.factory import make_aggregator, make_combinator
-from src.core.use_cases.keyword_scouter import KeywordsScouter
 from src.core.use_cases.similarity_filter import SimilarityFilter
-from src.core.use_cases.survey_orchestrator import SurveyOrchestrator
+from src.core.use_cases.survey_aggregator import SurveyAggregator
+from src.core.use_cases.survey_combinations import SurveyCombinations
 
 
 @mark.asyncio
 async def test_make_combinator():
     use_case = make_combinator()
+    api_gateway = getattr(use_case, "_api_gateway")
+    http_client = getattr(api_gateway, "_http_client")
 
-    use_case_builder = getattr(use_case, "_url_builder")
-    search_api = getattr(use_case, "_search_api")
-    use_case_details = getattr(use_case, "_details")
+    assert use_case and isinstance(use_case, SurveyCombinations)
+    assert api_gateway and isinstance(api_gateway, ScopusVolumeScouter)
+    assert http_client and isinstance(http_client, HTTPClient)
+    assert getattr(api_gateway, "_url_builder") is None
 
-    client: HTTPClient = getattr(search_api, "_http_client")  # type: ignore
-    await client.close()
-    search_api_builder = getattr(search_api, "_url_builder")
-    search_api_details = getattr(search_api, "_details")
-    state = getattr(search_api, "_state")
-
-    assert use_case and isinstance(use_case, KeywordsScouter)
-    assert search_api and isinstance(search_api, ScopusSearchAPI)
-    assert client and isinstance(client, HTTPClient)
-    assert search_api_builder and search_api_details
-    assert use_case_builder and isinstance(use_case_builder, URLBuilder)
-    assert id(use_case_builder) == id(search_api_builder)
-    assert use_case_details and isinstance(use_case_details, SurveyDetails)
-    assert id(use_case_details) == id(search_api_details)
-    assert state is None
+    await http_client.close()
 
 
 # mypy: disable-error-code="annotation-unchecked"
 @mark.asyncio
 async def test_make_aggregator():
     use_case = make_aggregator()
+    api_gateway = getattr(use_case, "_api_gateway")
+    sim_filter = getattr(use_case, "_similarity_filter")
+    http_client = getattr(api_gateway, "_http_client")
+    context = getattr(api_gateway, "_ctx")
 
-    search_api = getattr(use_case, "_search_api")
-    abstract_api = getattr(use_case, "_abstract_api")
-    similarity_filter = getattr(use_case, "_similarity_filter")
-    use_case_details = getattr(use_case, "_details")
+    assert use_case and isinstance(use_case, SurveyAggregator)
+    assert api_gateway and isinstance(api_gateway, ScopusDatasetGatherer)
+    assert sim_filter and isinstance(sim_filter, SimilarityFilter)
+    assert http_client and isinstance(http_client, HTTPClient)
+    assert context and isinstance(context, ScopusContext)
 
-    search_client: HTTPClient = getattr(search_api, "_http_client")
-    await search_client.close()
-    search_builder = getattr(search_api, "_url_builder")
-    search_details = getattr(search_api, "_details")
-    search_state = getattr(search_api, "_state")
+    assert getattr(api_gateway, "_search_url_builder") is None
+    assert getattr(api_gateway, "_abstract_url_builder") is None
 
-    abstract_client: HTTPClient = getattr(abstract_api, "_http_client")
-    await abstract_client.close()
-    abstract_builder = getattr(abstract_api, "_url_builder")
-    abstract_details = getattr(abstract_api, "_details")
-    abstract_state = getattr(abstract_api, "_state")
-
-    assert use_case and isinstance(use_case, SurveyOrchestrator)
-    assert similarity_filter and isinstance(
-        similarity_filter, SimilarityFilter
-    )
-    assert search_api and isinstance(search_api, ScopusSearchAPI)
-    assert abstract_api and isinstance(
-        abstract_api, ScopusAbstractRetrievalAPI
-    )
-    assert abstract_builder and abstract_details and use_case_details
-
-    assert search_client and isinstance(search_client, HTTPClient)
-    assert abstract_client and isinstance(abstract_client, HTTPClient)
-    assert id(search_client) == id(abstract_client)
-
-    assert search_builder and isinstance(search_builder, URLBuilder)
-    assert id(search_builder) == id(abstract_builder)
-
-    assert search_details and isinstance(search_details, SurveyDetails)
-    assert id(search_details) == id(abstract_details) == id(use_case_details)
-
-    assert search_state and isinstance(search_state, SurveyState)
-    assert id(search_state) == id(abstract_state)
+    await http_client.close()
