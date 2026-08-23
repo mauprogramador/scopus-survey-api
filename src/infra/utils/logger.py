46e3e259892ad4edd0d932a408cf112adb036af8
mauprogramador <sir.silvabmauricio@gmail.com>
2026-08-23 03:32:39 -0400

    feat: move log messages to where they belong and add trace class

--- src/infra/utils/logger.py
+++ src/infra/utils/logger.py
@@ -145,15 +145,11 @@ class _TQDMLoggingHandler(logging.StreamHandler):
             self.handleError(record)
 
 
-_METHOD_COLOR = {"GET": "94", "POST": "92", "PUT": "93", "DELETE": "91"}
-_FRAME = traceback.FrameSummary(
-    filename=__file__, lineno=1, name="<logging>", colno=1
-)
-_STATUS_COLOR = {2: "32", 3: "33", 4: "31", 5: "31"}
 _FMT = "%(asctime)s %(levelname)-18s %(message)s"
+_DATEFMT = "%Y-%m-%d %H:%M:%S"  # e.g. 2026-01-01 00:00:00
+
 _FILENAME = Path(f".logs/{_FileHandler.filename(0)}")
 _LOGGER_NAME = "scopus.survey.api"
-_DATEFMT = "%Y-%m-%d %H:%M:%S"  # e.g. 2026-01-01 00:00:00
 
 FILE_HANDLER: Json = {
     "()": _FileHandler,
@@ -200,7 +196,6 @@ _LOGGING_CONFIG: Json = {
     },
 }
 
-
 if ENV.logging_file:
     _FILENAME.parent.mkdir(exist_ok=True)
     _LOGGING_CONFIG["handlers"]["file"] = FILE_HANDLER
@@ -215,50 +210,15 @@ logging.config.dictConfig(_LOGGING_CONFIG)
 LOGGER = logging.getLogger(_LOGGER_NAME)
 
 
-_QUOTA = (
-    "Scopus API: \033[32m%(api_name)s\033[m. Limit: \033[33m%(limit)s\033[m. "
-    "Remaining: \033[33m%(remaining)s\033[m. Reset: \033[33m%(reset)s"
-    "\033[m. ELS-Status: \033[33m%(status)s\033[m"
-)
-_TRACE = (
-    "[\033[36m%(host)s\033[m:\033[36m%(port)d\033[m] \033[%(method_color)sm"
-    "%(method)s \033[37;1m%(url)s\033[m \033[%(status_color)sm%(code)d "
-    "%(status_phrase)s \033[m%(time)s\033[m"
-)
-_COMBINATIONS = (
-    "Keywords: \033[33m%(keywords)d\033[m. Combinations: \033[33m"
-    "%(combinations)d\033[m"
-)
-_LOSS = (
-    "Initial: \033[33m%(initial)d\033[m. Final: \033[33m%(final)d"
-    "\033[m. Loss: \033[33m%(loss_amount)d \033[m(\033[33m"
-    "%(loss_percent).2f%%\033[m)"
-)
-_EXCEPTION = (
-    '\033[31m%(qualname)s\033[m: File "%(filepath)s", line %(line)d, col '
-    "%(col)d, from \033[31m%(module)s.%(qualname)s\033[m"
-)
-_PROD_CONFIG = (
-    "Active Configuration (Production-only Forced Overrides Applied): %s\033[m"
-)
-_GUNICORN_RUNNING = (
-    "Gunicorn running at\033[37;1m http://localhost:%d"
-    "\033[m (Press CTRL+C to quit)\033[m"
-)
-_LOCALHOST_ACCESS = "Please access at \033[37;1mhttp://localhost:%d\033[m"
-_TRY_AGAIN = "Please try again on \033[37;1m%s\033[m"
-_BATCH_TO_FETCH = "Batch of \033[33m%d\033[m requests to fetch"
-_BATCH_COMPLETED = "Batch completed in \033[33m%s\033[m"
-_TOTAL_FOUND = "Total Found: \033[33m%d\033[m"
-
-
 def info(message: str) -> None:
     LOGGER.info("%s\033[m", message, stacklevel=2)
 
 
 def batch_to_fetch(progress: range) -> None:
     LOGGER.info(
-        _BATCH_TO_FETCH, (progress.stop - progress.start), stacklevel=2
+        "Batch of \033[33m%d\033[m requests to fetch\033[m",
+        (progress.stop - progress.start),
+        stacklevel=2,
     )
 
 
@@ -268,7 +228,8 @@ def batch_completed(process_time: float) -> None:
         duration = f"{process_time:.2f}s ({minutes:.2f}m)"
     else:
         duration = f"{process_time:.2f}s"
-    LOGGER.info(_BATCH_COMPLETED, duration, stacklevel=2)
+
+    LOGGER.info("Batch completed in \033[33m%s\033[m", duration, stacklevel=2)
 
 
 def loss(initial: int, final: int) -> None:
@@ -280,7 +241,13 @@ def loss(initial: int, final: int) -> None:
         "loss_amount": loss_value,
         "loss_percent": loss_percent,
     }
-    LOGGER.info(_LOSS, args, stacklevel=2)
+    LOGGER.info(
+        "Initial: \033[33m%(initial)d\033[m. Final: \033[33m%(final)d"
+        "\033[m. Loss: \033[33m%(loss_amount)d \033[m(\033[33m"
+        "%(loss_percent).2f%%\033[m)",
+        args,
+        stacklevel=2,
+    )
 
 
 def combinations(keywords_count: int) -> None:
@@ -288,7 +255,12 @@ def combinations(keywords_count: int) -> None:
         "keywords": keywords_count,
         "combinations": 2**keywords_count - 1,
     }
-    LOGGER.info(_COMBINATIONS, args, stacklevel=2)
+    LOGGER.info(
+        "Keywords: \033[33m%(keywords)d\033[m. Combinations: \033[33m"
+        "%(combinations)d\033[m",
+        args,
+        stacklevel=2,
+    )
 
 
 def quota(
@@ -304,37 +276,57 @@ def quota(
         "reset": headers.reset_datetime,
         "status": headers.status,
     }
-    LOGGER.log(_Level.QUOTA, _QUOTA, args, stacklevel=2)
+    LOGGER.log(
+        _Level.QUOTA,
+        "Scopus API: \033[32m%(api_name)s\033[m. Limit: \033[33m%(limit)s"
+        "\033[m. Remaining: \033[33m%(remaining)s\033[m. Reset: \033[33m"
+        "%(reset)s\033[m. ELS-Status: \033[33m%(status)s\033[m",
+        args,
+        stacklevel=2,
+    )
 
 
 def try_again(reset: str) -> None:
-    LOGGER.info(_TRY_AGAIN, reset, stacklevel=2)
+    LOGGER.info("Please try again on \033[37;1m%s\033[m", reset, stacklevel=2)
 
 
 def localhost(port: int) -> None:
-    LOGGER.info(_LOCALHOST_ACCESS, port, stacklevel=2)
+    LOGGER.info(
+        "Please access at \033[37;1mhttp://localhost:%d\033[m",
+        port,
+        stacklevel=2,
+    )
 
 
 def total_found(total_results: int) -> None:
-    LOGGER.info(_TOTAL_FOUND, total_results, stacklevel=2)
+    LOGGER.info("Total Found: \033[33m%d\033[m", total_results, stacklevel=2)
 
 
 def gunicorn_running(env_config: Json) -> None:
-    prod_config = to_jsonable_python(env_config, fallback=repr)
-    LOGGER.info(_PROD_CONFIG, prod_config, stacklevel=2)
-    LOGGER.info(_GUNICORN_RUNNING, env_config["port"], stacklevel=2)
+    LOGGER.info(
+        "Active Configuration (Production-only Forced Overrides Applied): "
+        "%s\033[m",
+        to_jsonable_python(env_config, fallback=repr),
+        stacklevel=2,
+    )
+    LOGGER.info(
+        "Gunicorn running at\033[37;1m http://localhost:%d"
+        "\033[m (Press CTRL+C to quit)\033[m",
+        env_config["port"],
+        stacklevel=2,
+    )
 
 
 def error(message: str, tracking_id: str = None) -> None:
-    if tracking_id:
+    if not tracking_id:
+        LOGGER.error("\033[31m%s\033[m", message, stacklevel=2)
+    else:
         LOGGER.error(
             "\033[31m%s\033[m [ID:\033[36m%s\033[m]",
             message,
             tracking_id,
             stacklevel=2,
         )
-    else:
-        LOGGER.error("\033[31m%s\033[m", message, stacklevel=2)
 
 
 def debug(**kwargs) -> None:
@@ -355,8 +347,14 @@ def debug(**kwargs) -> None:
 
 
 def exception(exc: Exception) -> None:
-    exc_trace = sys.exc_info()[2]
-    frame = traceback.extract_tb(exc_trace)[-1] if exc_trace else _FRAME
+    exc_trace = exc.__traceback__
+
+    if exc_trace is not None:
+        frame = traceback.extract_tb(exc_trace)[-1]
+    else:
+        frame = traceback.FrameSummary(
+            filename="unknown", lineno=0, name="unknown", colno=0
+        )
 
     args = {
         "module": type(exc).__module__,
@@ -365,40 +363,62 @@ def exception(exc: Exception) -> None:
         "line": frame.lineno if frame.lineno else 1,
         "col": frame.colno if frame.colno else 1,
     }
-    LOGGER.log(_Level.EXCEPTION, _EXCEPTION, args, exc_info=True, stacklevel=2)
+    LOGGER.log(
+        _Level.EXCEPTION,
+        '\033[31m%(qualname)s\033[m: File "%(filepath)s", line %(line)d, col '
+        "%(col)d, from \033[31m%(module)s.%(qualname)s\033[m",
+        args,
+        exc_info=True,
+        stacklevel=2,
+    )
 
 
-# e.g. apiKey=6bd9327547a3cf4c56586324df4b7d92  (Random Hash)
-_API_KEY_PARAM_RE = re.compile(r"apiKey\=([a-zA-Z0-9]{32})")
+class _Trace:
+    METHOD_COLOR = {"GET": "94", "POST": "92", "PUT": "93", "DELETE": "91"}
+    STATUS_COLOR = {2: "32", 3: "33", 4: "31", 5: "31"}
+    # e.g. apiKey=6bd9327547a3cf4c56586324df4b7d92  (Random Hash)
+    API_KEY_PARAM_RE = re.compile(r"apiKey\=([a-zA-Z0-9]{32})")
 
+    @classmethod
+    def _replacer(cls, match: re.Match[str]) -> str:
+        return f"apiKey={mask_secret(match.group(1))}"
 
-def _mask_api_key_param(match: re.Match[str]) -> str:
-    return f"apiKey={mask_secret(match.group(1))}"
+    @classmethod
+    def mask_api_key_param(cls, url: str) -> str:
+        return cls.API_KEY_PARAM_RE.sub(cls._replacer, url)
 
+    @classmethod
+    def log(
+        cls,
+        prefix: _Level,
+        req: FastAPIRequest,
+        code: int,
+        time: str,
+    ) -> None:
+        if req.client is None:
+            host, port = ENV.host, ENV.port
+        else:
+            host, port = req.client.host, req.client.port
 
-def _trace(
-    prefix: _Level,
-    req: FastAPIRequest,
-    code: int,
-    time: str,
-) -> None:
-    if req.client is None:
-        host, port = ENV.host, ENV.port
-    else:
-        host, port = req.client.host, req.client.port
-
-    args = {
-        "host": host,
-        "port": port,
-        "method_color": _METHOD_COLOR.get(req.method, "90"),
-        "method": req.method,
-        "url": _API_KEY_PARAM_RE.sub(_mask_api_key_param, str(req.url)),
-        "status_color": _STATUS_COLOR[(code // 100)],
-        "code": code,
-        "status_phrase": HTTPStatus(code).phrase,
-        "time": time,
-    }
-    LOGGER.log(prefix, _TRACE, args, stacklevel=3)
+        args = {
+            "host": host,
+            "port": port,
+            "method_color": cls.METHOD_COLOR.get(req.method, "90"),
+            "method": req.method,
+            "url": cls.mask_api_key_param(str(req.url)),
+            "status_color": cls.STATUS_COLOR.get((code // 100), "30"),
+            "code": code,
+            "status_phrase": HTTPStatus(code).phrase,
+            "time": time,
+        }
+        LOGGER.log(
+            prefix,
+            "[\033[36m%(host)s\033[m:\033[36m%(port)d\033[m] \033["
+            "%(method_color)sm%(method)s \033[37;1m%(url)s\033[m \033["
+            "%(status_color)sm%(code)d %(status_phrase)s \033[m%(time)s\033[m",
+            args,
+            stacklevel=3,
+        )
 
 
 def trace(req: FastAPIRequest, code: int, process_time: float) -> None:
@@ -407,7 +427,7 @@ def trace(req: FastAPIRequest, code: int, process_time: float) -> None:
         duration = f"{process_time:.2f}s ({minutes:.2f}m)"
     else:
         duration = f"{process_time:.2f}s"
-    _trace(_Level.ACCESS, req, code, duration)
+    _Trace.log(_Level.ACCESS, req, code, duration)
 
 
 def api_call(url: str, code: int, time: float) -> None:
@@ -417,4 +437,4 @@ def api_call(url: str, code: int, time: float) -> None:
         "path": url,
         "headers": {},
     }
-    _trace(_Level.API_CALL, FastAPIRequest(scope), code, f"{time:.2f}s")
+    _Trace.log(_Level.API_CALL, FastAPIRequest(scope), code, f"{time:.2f}s")
