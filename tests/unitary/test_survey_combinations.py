c992257a57fa62dd287a027ad86eae1c4dca2a79
mauprogramador <sir.silvabmauricio@gmail.com>
2026-08-24 02:15:33 -0400

    test: use patch dict to mock combination query params

--- tests/unitary/test_survey_combinations.py
+++ tests/unitary/test_survey_combinations.py
@@ -2,13 +2,14 @@ import random
 from unittest.mock import AsyncMock
 
 from pytest import mark
+from pytest_mock import MockerFixture as Mocker
 
 from src.adapters.gateway.scopus_volume_scouter import ScopusVolumeScouter
 from src.adapters.serializers.query_params import CombinationParams
 from src.adapters.serializers.scopus_data import ScopusHeaders
-from src.adapters.types import Button, TotalBundle
+from src.adapters.types import TotalBundle
 from src.core.use_cases.survey_combinations import SurveyCombinations
-from tests.mocks.raw import API_KEY, KEYWORDS, RAW_HEADERS_OK
+from tests.mocks.raw import ALIAS_COMBINATION_PARAMS, KEYWORDS, RAW_HEADERS_OK
 
 
 async def _mock_fetch(
@@ -35,51 +36,38 @@ USE_CASE = SurveyCombinations(
 
 
 @mark.asyncio
-async def test_survey_two_keywords():
-    raw = {
-        "api_key": API_KEY,
-        "keywords": KEYWORDS[:2],
-        "button": Button.COMBINATION.value,
-    }
-    params = CombinationParams(**raw)
+async def test_survey_two_keywords(mocker: Mocker):
+    mocker.patch.dict(ALIAS_COMBINATION_PARAMS, {"keywords": KEYWORDS[:2]})
+    params = CombinationParams(**ALIAS_COMBINATION_PARAMS)
     results, _ = await USE_CASE.process(params)
 
     assert len(results) == 3 and sum(item["total"] for item in results) > 0
     combinations_repr = ";".join(item["combination"] for item in results)
 
-    for keyword in raw["keywords"]:
+    for keyword in ALIAS_COMBINATION_PARAMS["keywords"]:
         assert keyword in combinations_repr
 
 
 @mark.asyncio
-async def test_survey_three_keywords():
-    raw = {
-        "api_key": API_KEY,
-        "keywords": KEYWORDS[:3],
-        "button": Button.COMBINATION.value,
-    }
-    params = CombinationParams(**raw)
+async def test_survey_three_keywords(mocker: Mocker):
+    mocker.patch.dict(ALIAS_COMBINATION_PARAMS, {"keywords": KEYWORDS[:3]})
+    params = CombinationParams(**ALIAS_COMBINATION_PARAMS)
     results, _ = await USE_CASE.process(params)
 
     assert len(results) == 7 and sum(item["total"] for item in results) > 0
     combinations_repr = ";".join(item["combination"] for item in results)
 
-    for keyword in raw["keywords"]:
+    for keyword in ALIAS_COMBINATION_PARAMS["keywords"]:
         assert keyword in combinations_repr
 
 
 @mark.asyncio
 async def test_survey_four_keywords():
-    raw = {
-        "api_key": API_KEY,
-        "keywords": KEYWORDS,
-        "button": Button.COMBINATION.value,
-    }
-    params = CombinationParams(**raw)
+    params = CombinationParams(**ALIAS_COMBINATION_PARAMS)
     results, _ = await USE_CASE.process(params)
 
     assert len(results) == 15 and sum(item["total"] for item in results) > 0
     combinations_repr = ";".join(item["combination"] for item in results)
 
-    for keyword in raw["keywords"]:
+    for keyword in ALIAS_COMBINATION_PARAMS["keywords"]:
         assert keyword in combinations_repr
